8a29a15c73f899b2366c707cd3774cdc50158fd6
Furqan Dar <darf@kenyon.edu>
2023-12-05 10:30:22 -0600

    Included a convenient wrapper to directly extract data from .nii or .nii.gz images as numpy arrays. Added a function that iterates over multiples images to return the average value over a mask.

--- image_derived_input_function.py
+++ image_derived_input_function.py
@@ -4,25 +4,57 @@ import nibabel
 
 def extract_from_nii_as_numpy(file_path: str) -> np.ndarray:
     """
-    
-    :param file_path:
-    :return:
+    Convenient wrapper to extract data from a .nii or .nii.gz file as a numpy array
+    :param file_path: The full path to the file.
+    :return: The data contained in the .nii or .nii.gz file as a numpy array
     """
     
     return nibabel.load(filename=file_path).get_fdata()
     
 
-
 def compute_average_over_mask(mask: np.ndarray, image: np.ndarray) -> float:
     """
-    
+    We compute the average value of `image` over the provided `mask`. We multiply the mask
+    and the image element-wise, which gives us the image pixels corresponding to the mask. We then take the standard mean
+    over those values by summing and dividing by the number of values. Note that the implementation does not care about
+    the dimensions of mask and image; just that they be the same.
     :param mask: 3D numpy array that contains 1s and 0s.
     :param image: 3D numpy array corresponding to an image.
-    :return: np.sum(mask * image) / np.sum(mask). The average value of image over the provided mask. We multiply the mask
-    and the image element-wise, which gives us the image pixels corresponding to the mask. We then take the standard mean
-    over those values by summing and dividing by the number of values.
+    :return: Average value of the image over the mask.
     """
-    # TODO: Move this assert to a higher level function if speed is greatly affected by this when iterating over many frames
+    #TODO: Move this assert to a higher level function if speed is greatly affected by this when iterating over many frames
     assert mask.shape == image.shape, "The mask and the image should have the same dimensions."
     
     return np.sum(mask * image) / np.sum(mask)
+
+
+def compute_average_over_mask_of_multiple_frames(mask: np.ndarray,
+                                                 image_series: np.ndarray,
+                                                 start: int,
+                                                 stride: int,
+                                                 end: int):
+    """
+    Given a mask, and multiple images, we compute the average value of the image over the mask. It is assumed that the last
+    index for the images corresponds to different images or time-points. A simple and explicit iterator to stride over the
+    images is included.
+    :param mask:
+    :param image_series:
+    :param start:
+    :param stride:
+    :param end:
+    :return:
+    """
+    #TODO: A smarter way to stride over images so that negative indecies can be used.
+    assert start >= 0, "`start` has to be >= 0."
+    assert end < image_series.shape[-1], "`end` has to be smaller than the number of frames in the image array."
+    assert stride >= 0, "`stride` has to be >= 0."
+    
+    frame_it = range(start, end, stride)
+    num_frames = len(frame_it)
+    avg_values = np.zeros(num_frames, float)
+    
+    for t, frmID in enumerate(frame_it):
+        avg_values[t] = compute_average_over_mask(mask=mask, image=image_series[:, :, :, frmID])
+    
+    return avg_values
+    
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import image_derived_input_function
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+import image_derived_input_function as idif
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    carotid_mask = idif.extract_from_nii_as_numpy(
+        file_path="/Users/furqandar/Desktop/Work/BrierLab/PracticeData/1179306_v1/car1_2.nii.gz")
+    print(carotid_mask.shape)
+    
+    pvc_images = idif.extract_from_nii_as_numpy(
+        file_path="/Users/furqandar/Desktop/Work/BrierLab/PracticeData/1179306_v1/1179306_v1_STC_PVC.nii.gz")
+    print(pvc_images.shape)
+    
+    print(idif.compute_average_over_mask(mask=carotid_mask, image=pvc_images[:, :, :, -1]))
+    
+    avg_vals = idif.compute_average_over_mask_of_multiple_frames(mask=carotid_mask, image_series=pvc_images, start=0,
+                                                                 stride=1, end=40)
+    
+    print(avg_vals)
