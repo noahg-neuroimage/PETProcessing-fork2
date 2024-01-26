35e665fbfc8f61438e7d835e9185a2d782080f40
Furqan Dar <darf@kenyon.edu>
2024-01-26 16:42:44 -0600

    Added a simple check to crash if threshold is too large. Also, beginning to refactor out the cumulative integral function since SciPy has a function that does that.

--- pet_cli/parametric_images.py
+++ pet_cli/parametric_images.py
@@ -1,6 +1,7 @@
 import nibabel
 import numpy as np
 import numba
+from scipy.integrate import cumulative_trapezoid
 
 from typing import Tuple
 
@@ -31,20 +32,20 @@ def line_fit(xdata: np.ndarray, ydata: np.ndarray) -> float:
     fit_ans = np.linalg.lstsq(matrix, ydata)[0]
     return fit_ans
 
-
+# TODO: Remove the function since SciPy has a version.
 # TODO: Add documentation
 # TODO: Should the zeroth index be 0?
 @numba.njit()
-def calculate_cumulative_integral(function_vals: np.ndarray, function_times: np.ndarray) -> np.ndarray:
+def calculate_cumulative_integral(y: np.ndarray, x: np.ndarray) -> np.ndarray:
     """
     
-    :param function_vals:
-    :param function_times:
+    :param y:
+    :param x:
     :return:
     """
-    integral = np.zeros_like(function_vals)
-    for i in range(1, len(function_vals)):
-        integral[i] = np.trapz(y=function_vals[1:i], x=function_times[1:i])
+    integral = np.zeros_like(y)
+    for i in range(1, len(y)):
+        integral[i-1] = np.trapz(y=y[0:i], x=x[0:i])
     return integral
 
 
@@ -54,15 +55,7 @@ def generate_parametric_image_with_patlak(intensity_image: np.ndarray,
                                           input_func: np.ndarray,
                                           input_times: np.ndarray,
                                           t_thresh: int) -> np.ndarray:
-    """
-    
-    :param intensity_image:
-    :param input_func:
-    :param input_times:
-    :param t_thresh:
-    :return:
-    """
-    input_int = calculate_cumulative_integral(input_func, input_times)
+    input_int = np.array(cumulative_trapezoid(y=input_func, x=input_times, initial=0.0), float)
     x_var = input_int[t_thresh:] / input_func[t_thresh:]
     image_shape = intensity_image.shape
     x_dim = image_shape[0]
@@ -84,19 +77,31 @@ def read_tsv_tac(file_path: str) -> Tuple[np.ndarray, np.ndarray]:
     return np.array(times, float), np.array(tac, float)
 
 #TODO: Add documentation
-def save_parametric_image_from_4DPET_using_patlak(pet_file: str, input_func_file: str, out_file: str, thresh_in_mins: float):
+def save_parametric_image_from_4DPET_using_patlak(pet_file: str,
+                                                  input_func_file: str,
+                                                  out_file: str,
+                                                  thresh_in_mins: float,
+                                                  verbose=True):
     """
     
     :param pet_file:
     :param input_func_file:
     :param out_file:
     :param thresh_in_mins:
+    :param verbose:
     :return:
     """
     pet_file = nibabel.load(filename=pet_file)
     
     input_times, input_tac = read_tsv_tac(input_func_file)
-    t_thresh = np.argwhere(input_times >= thresh_in_mins)[0, 0]
+    try:
+        t_thresh = np.argwhere(input_times >= thresh_in_mins)[0, 0]
+    except IndexError:
+        print(f"There are no data for t>=t* ({thresh_in_mins} minutes). Consider lowering the threshold.")
+        raise IndexError
+    
+    if verbose:
+        print(f"{len(input_times)-t_thresh} points will be fit for each TAC.")
     
     parametric_values = generate_parametric_image_with_patlak(intensity_image=pet_file.get_fdata()/37000.,
                                                               input_func=input_tac,
