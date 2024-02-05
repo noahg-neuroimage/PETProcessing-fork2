aff7d548d848e842cccdc9e447e9badfe427aade
Furqan Dar <darf@kenyon.edu>
2024-02-05 12:15:39 -0600

    Implemented a function to generate the RHS matrix of linear least squares from an array of independent variable values.

--- pet_cli/graphical_analysis.py
+++ pet_cli/graphical_analysis.py
@@ -5,3 +5,20 @@
 
 __version__ = '0.1'
 
+import numpy as np
+import numba
+import typing
+
+@numba.njit()
+def _line_fitting_make_rhs_matrix_from_xdata(xdata: numba.float64[::1]) -> numba.float64[:, ::1]:
+    """Generates the RHS matrix for linear least squares fitting
+
+    Args:
+        xdata (numba.float64): array of independent variable values
+
+    Returns:
+        2D matrix where first column is `xdata` and the second column is 1s.
+    """
+    out_matrix = np.ones((len(xdata), 2), float)
+    out_matrix[:, 0] = xdata
+    return out_matrix
