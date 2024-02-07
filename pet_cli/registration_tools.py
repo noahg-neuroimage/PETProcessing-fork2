4425005fd8c1120faa03cf4b0e5e4a667eac1446
B-Judge <bradley.a.judge@gmail.com>
2024-02-06 19:15:40 -0600

    Added MR bias field correction. Made edits to expected image types: FileBaseImages is the general image class, nibabel.nifti1.Nifti1Image is the expected file type after loading.

--- pet_cli/registration_tools.py
+++ pet_cli/registration_tools.py
@@ -1,16 +1,18 @@
 import nibabel.nifti1
 from nibabel.filebasedimages import FileBasedHeader, FileBasedImage
 import numpy as np
+import SimpleITK as sitk
 
 
 def load_nii(file_path: str, verbose: bool) -> FileBasedImage:
-    """
+    """Wrapper to load nifti from file path.
 
     Args:
-        file_path:
+        file_path: The full file path to the nifti file.
         verbose:
 
     Returns:
+        The nifti FileBasedImage.
 
     """
     image = nibabel.load(file_path)
@@ -21,52 +23,82 @@ def load_nii(file_path: str, verbose: bool) -> FileBasedImage:
     return image
 
 
-def extract_image_from_nii_as_numpy(file: FileBasedImage, verbose: bool) -> np.ndarray:
+def extract_image_from_nii_as_numpy(image: nibabel.nifti1.Nifti1Image, verbose: bool) -> np.ndarray:
     """Convenient wrapper to extract data from a .nii or .nii.gz file as a numpy array.
 
     Args:
-        file: The full path to the file or the nifti file itself.
+        image: The nifti file itself.
         verbose:
 
     Returns:
-        The data contained in the .nii or .nii.gz file as a numpy array
+        The data contained in the .nii or .nii.gz file as a numpy array.
 
     """
-    image_data: np.ndarray = file.get_fdata()
+    image_data: np.ndarray = image.get_fdata()
 
     if verbose:
-        print(f"(fileIO): {file} has shape {image_data.shape}")
+        print(f"(fileIO): Image has shape {image_data.shape}")
 
     return image_data
 
 
-def extract_header_from_nii(file: FileBasedImage, verbose: bool) -> FileBasedHeader:
-    """Convenient wrapper to extract header information from a .nii or .nii.gz file as a nibabel file-based header
+def extract_header_from_nii(image: nibabel.nifti1.Nifti1Image, verbose: bool) -> FileBasedHeader:
+    """Convenient wrapper to extract header information from a .nii or .nii.gz file as a nibabel file-based header.
 
     Args:
-        file: The full path to the file or the nifti file itself
+        image: The nifti file itself.
         verbose:
 
     Returns:
-        The nifti header
+        The nifti header.
 
     """
-    image_header: FileBasedHeader = file.header
+    image_header: FileBasedHeader = image.header
 
     if verbose:
-        print(f"(fileIO): {file} header is: {image_header}")
+        print(f"(fileIO): Image header is: {image_header}")
 
     return image_header
 
 
-def reorient_to_ras(file: FileBasedImage, verbose: bool) -> FileBasedImage:
-    """
+def reorient_to_ras(image: nibabel.nifti1.Nifti1Image, verbose: bool) -> nibabel.nifti1.Nifti1Image:
+    """Wrapper for the RAS reorientation used to ensure images are oriented the same.
 
     Args:
-        file_path:
+        image: The nifti file itself.
+        verbose:
+
+    Returns:
+        The reoriented nifti file.
+
+    """
+    reoriented_image: nibabel.nifti1.Nifti1Image = nibabel.as_closest_canonical(image)
+
+    if verbose:
+        print(f"(fileIO): Image has been reoriented to RAS")
+
+    return reoriented_image
+
+
+def apply_3d_mr_bias_correction(image: nibabel.nifti1.Nifti1Image, verbose: bool) -> nibabel.nifti1.Nifti1Image:
+    """Wrapper for bias field correction on 3-dimensional MR images, may use N4BiasFieldCorrection
+
+    Args:
+        image:
         verbose:
 
     Returns:
 
     """
+    image_array = extract_image_from_nii_as_numpy(image=image, verbose=False)
+    image_header = extract_header_from_nii(image=image, verbose=False)
+    sitk_image = sitk.GetImageFromArray(np.array(image_array, dtype=image_array.dtype))
+    sitk_image.SetSpacing(image_header.get_zooms()[:3])
 
+    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+    corrected_sitk_image = corrector.Execute(sitk_image)
+
+    corrected_img_array = sitk.GetArrayFromImage(corrected_sitk_image)
+    corrected_nibabel_image: nibabel.nifti1.Nifti1Image = nibabel.Nifti1Image(corrected_img_array, affine=image.affine)
+
+    return corrected_nibabel_image
