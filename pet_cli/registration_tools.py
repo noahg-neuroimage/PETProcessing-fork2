a12968d2f6ab792af52c183b0cd296f0753a7dee
B-Judge <bradley.a.judge@gmail.com>
2024-02-06 18:21:44 -0600

    Changed expected inputs to only handle FileBasedImage-s on the expectation that they will be passed already loaded nifti-s as input.

--- pet_cli/registration_tools.py
+++ pet_cli/registration_tools.py
@@ -1,7 +1,6 @@
 import nibabel.nifti1
 from nibabel.filebasedimages import FileBasedHeader, FileBasedImage
 import numpy as np
-from typing import Union
 
 
 def load_nii(file_path: str, verbose: bool) -> FileBasedImage:
@@ -17,12 +16,12 @@ def load_nii(file_path: str, verbose: bool) -> FileBasedImage:
     image = nibabel.load(file_path)
 
     if verbose:
-        print(f"(fileIO): {file} loaded")
+        print(f"(fileIO): {file_path} loaded")
 
     return image
 
 
-def extract_image_from_nii_as_numpy(file: Union[str, FileBasedImage], verbose: bool) -> np.ndarray:
+def extract_image_from_nii_as_numpy(file: FileBasedImage, verbose: bool) -> np.ndarray:
     """Convenient wrapper to extract data from a .nii or .nii.gz file as a numpy array.
 
     Args:
@@ -33,12 +32,10 @@ def extract_image_from_nii_as_numpy(file: Union[str, FileBasedImage], verbose: b
         The data contained in the .nii or .nii.gz file as a numpy array
 
     """
-    if isinstance(file, str):
-        image_data: np.ndarray = nibabel.load(filename=file).get_fdata()
-    elif isinstance(file, FileBasedImage):
+    if isinstance(file, FileBasedImage):
         image_data: np.ndarray = file.get_fdata()
     else:
-        raise TypeError("file must be a file path (str) or a Nifti1Image")
+        raise TypeError("file must be a Nifti1Image")
 
     if verbose:
         print(f"(fileIO): {file} has shape {image_data.shape}")
@@ -46,7 +43,7 @@ def extract_image_from_nii_as_numpy(file: Union[str, FileBasedImage], verbose: b
     return image_data
 
 
-def extract_header_from_nii(file: Union[str, FileBasedImage], verbose: bool) -> FileBasedHeader:
+def extract_header_from_nii(file: FileBasedImage, verbose: bool) -> FileBasedHeader:
     """Convenient wrapper to extract header information from a .nii or .nii.gz file as a nibabel file-based header
 
     Args:
@@ -57,12 +54,10 @@ def extract_header_from_nii(file: Union[str, FileBasedImage], verbose: bool) -> 
         The nifti header
 
     """
-    if isinstance(file, str):
-        image_header: FileBasedHeader = nibabel.load(filename=file).header
-    elif isinstance(file, FileBasedImage):
+    if isinstance(file, FileBasedImage):
         image_header: FileBasedHeader = file.header
     else:
-        raise TypeError("file must be a file path (str) or a Nifti1Image")
+        raise TypeError("file must be a Nifti1Image")
 
     if verbose:
         print(f"(fileIO): {file} header is: {image_header}")
@@ -70,7 +65,7 @@ def extract_header_from_nii(file: Union[str, FileBasedImage], verbose: bool) -> 
     return image_header
 
 
-def reorient_to_ras(file: str, verbose: bool) -> FileBasedImage:
+def reorient_to_ras(file: FileBasedImage, verbose: bool) -> FileBasedImage:
     """
 
     Args:
