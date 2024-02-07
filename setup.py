c6d9f5d28c15cf6ea57467e2422e72304ba77e0f
B-Judge <bradley.a.judge@gmail.com>
2024-02-06 19:13:36 -0600

    Added SimpleITK to required package installations for access to N4BiasFieldCorrectionImageFilter for MR bias field corrections.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(name='pet_cli', version='0.0.1', packages=find_packages(),
-      install_requires=['numpy', 'scipy', 'numba', 'pandas', 'nibabel', 'antspyx'],
+      install_requires=['numpy', 'scipy', 'numba', 'pandas', 'nibabel', 'antspyx', 'SimpleITK'],
       entry_points={'console_scripts': ['pet-cli = pet_cli.cli:main', ], }, )
