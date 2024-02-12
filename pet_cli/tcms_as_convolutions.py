61626daebda6c3f746e2e00dd45e71a1b16a201a
Furqan Dar <darf@kenyon.edu>
2024-02-12 12:28:20 -0600

    Added the response function for the 1TCM.

--- pet_cli/tcms_as_convolutions.py
+++ pet_cli/tcms_as_convolutions.py
@@ -5,3 +5,17 @@
 import numpy as np
 import numba
 
+
+@numba.njit()
+def response_function_1tcm(t: np.ndarray[float], k1: float, k2: float) -> np.ndarray:
+    """The response function for the 1TCM :math:`f(t)=k_1 e^{-k_{2}t}`
+    
+    Args:
+        t (np.ndarray[float]): Array containing time-points where :math:`t\geq0`.
+        k1 (float): Rate constant for tranpsort from plasma/blood to tissue compartment
+        k2 (float): Rate constant for tranpsort from tissue compartment back to plasma/blood
+
+    Returns:
+        (np.ndarray[float]): Array containing response function values given the constants.
+    """
+    return k1 * np.exp(-k2 * t)
