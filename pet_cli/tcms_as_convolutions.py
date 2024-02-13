e6e3bc0186049e77ec0377331f5f81528c6af5b4
FurqanDar <dar@wustl.edu>
2024-02-13 16:36:32 -0600

    Added a function to calculate the TTAC of the second compartment given an input TAC for the 2TCM with k_4=0.

--- pet_cli/tcms_as_convolutions.py
+++ pet_cli/tcms_as_convolutions.py
@@ -203,9 +203,34 @@ def generate_tac_2tcm_with_k4zero_c1_from_tac(tac_times: np.ndarray[float],
         
     See Also:
         * :func:`response_function_2tcm_with_k4zero_c1` for more details about the 2TCM response function of the first compartment used for the convolution.
-        * :func:`response_function_2tcm_with_k4zero_c2` for more details about the 2TCM response function of the second compartment used for the convolution.
     """
     _resp_vals = response_function_2tcm_with_k4zero_c1(t=tac_times, k1=k1, k2=k2, k3=k3)
     dt = tac_times[1] - tac_times[0]
     c1 = calc_convolution_with_check(f=tac_vals, g=_resp_vals, dt=dt)
     return np.asarray([tac_times, c1])
+
+
+def generate_tac_2tcm_with_k4zero_c2_from_tac(tac_times: np.ndarray[float],
+                                              tac_vals: np.ndarray[float],
+                                              k1: float,
+                                              k2: float,
+                                              k3: float) -> np.ndarray[float, float]:
+    """Calculate the TTAC of the second comparment, given the input TAC, for a 2TCM (with :math:`k_{4}=0`) as an explicit convolution.
+    
+    Args:
+        tac_times (np.ndarray[float]): Array containing time-points where :math:`t\geq0` and equal time-steps.
+        tac_vals (np.ndarray[float]): Array containing TAC activities.
+        k1 (float): Rate constant for transport from plasma/blood to tissue compartment.
+        k2 (float): Rate constant for transport from first tissue compartment back to plasma/blood.
+        k3 (float): Rate constant for transport from tissue compartment to irreversible compartment.
+
+    Returns:
+        ((np.ndarray[float], np.ndarray[float])): Arrays containing the times and TTAC given the input TAC and parameters.
+        
+    See Also:
+        * :func:`response_function_2tcm_with_k4zero_c2` for more details about the 2TCM response function of the first compartment used for the convolution.
+    """
+    _resp_vals = response_function_2tcm_with_k4zero_c2(t=tac_times, k1=k1, k2=k2, k3=k3)
+    dt = tac_times[1] - tac_times[0]
+    c2 = calc_convolution_with_check(f=tac_vals, g=_resp_vals, dt=dt)
+    return np.asarray([tac_times, c2])
