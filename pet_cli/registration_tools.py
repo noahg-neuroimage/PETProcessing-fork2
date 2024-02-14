2721a85c52e75d499c5f8676b7e1bdf7338929e1
noahg-neuroimage <noahg@wustl.edu>
2024-02-14 14:05:49 -0600

    Moving from using nibabel to np.ndarray whenever possible. Still WIP. Don't expect this to work.

--- pet_cli/registration_tools.py
+++ pet_cli/registration_tools.py
@@ -2,7 +2,7 @@
 Image utilities
 """
 import ants
-import nibabel.nifti1
+import nibabel
 from nibabel.filebasedimages import FileBasedHeader, FileBasedImage
 import numpy as np
 
@@ -16,6 +16,8 @@ class ImageUtil():
         self.verbose = verbose
 
 
+
+    # File I/O functions: these will be in subclass ImageIO
     def load_nii(self) -> FileBasedImage:
         """
         Wrapper to load nifti from file path.
@@ -61,7 +63,7 @@ class ImageUtil():
             The data contained in the .nii or .nii.gz file as a numpy array.
 
         """
-        image_data: np.ndarray = image.get_fdata()
+        image_data = image.get_fdata()
 
         if self.verbose:
             print(f"(fileIO): Image has shape {image_data.shape}")
@@ -69,14 +71,13 @@ class ImageUtil():
         return image_data
 
 
-    def extract_header_from_nii(self, image: nibabel.nifti1) -> FileBasedHeader:
+    def extract_header_from_nii(self, image: np.ndarray) -> FileBasedHeader:
         """
         Convenient wrapper to extract header information from a .nii or .nii.gz 
         file as a nibabel file-based header.
 
         Args:
             image: The nifti file itself.
-            verbose:
 
         Returns:
             The nifti header.
@@ -87,9 +88,18 @@ class ImageUtil():
             print(f"(fileIO): Image header is: {image_header}")
 
         return image_header
+    
+
+    def extract_np_to_nibabel(self, image: np.ndarray, header: FileBasedHeader, affine: np.ndarray) -> nibabel.nifti1:
+        """
+        Wrapper to convert an image array into nibabel object.
+        
+        """
+
+        return 0
 
 
-    def reorient_to_ras(self, image: nibabel.nifti1) -> nibabel.nifti1:
+    def reorient_to_ras(self, image: np.ndarray) -> np.ndarray:
         """
         Wrapper for the RAS reorientation used to ensure images are oriented the same.
 
@@ -100,7 +110,7 @@ class ImageUtil():
         Returns:
             The reoriented nifti file.
         """
-        reoriented_image: nibabel.nifti1 = nibabel.as_closest_canonical(image)
+        reoriented_image: np.ndarray = nibabel.as_closest_canonical(image)
 
         if self.verbose:
             print("(fileIO): Image has been reoriented to RAS")
@@ -109,15 +119,15 @@ class ImageUtil():
 
 
     def rigid_registration_calc(self,
-                           moving_image: nibabel.nifti1,
-                           fixed_image: nibabel.nifti1,
+                           moving_image: np.ndarray,
+                           fixed_image: np.ndarray,
                            ) -> np.ndarray:
         """
         Register two images and return the transformation matrix
 
         Args:
-            moving_image (nibabel.nifti1): Image to be registered
-            fixed_image (nibabel.nifti1): Reference image to be registered to
+            moving_image (np.ndarray): Image to be registered
+            fixed_image (np.ndarray): Reference image to be registered to
 
         Returns:
             rigid_transform (np.ndarray): Rigid transform array
@@ -137,17 +147,17 @@ class ImageUtil():
     def apply_registration(self,
                       moving_image: nibabel.nifti1,
                       fixed_image: nibabel.nifti1,
-                      xfm_matrix: np.ndarray) -> nibabel.nifti1:
+                      xfm_matrix: np.ndarray) -> np.ndarray:
         """
         Register a moving image to a fixed image using a supplied transform.
 
         Args:
-            moving_image (nibabel.nifti1): Image to be resampled onto fixed reference image.
-            fixed_image (nibabel.nifti1): Reference image onto which the moving_image is registered.
+            moving_image (np.ndarray): Image to be resampled onto fixed reference image.
+            fixed_image (np.ndarray): Reference image onto which the moving_image is registered.
             xfm_matrix (np.ndarray): Ants-style transformation matrix used to apply transformation.
 
         Returns:
-            moving_on_fixed_image (nibabel.nifti1): Moving image registered onto the fixed image.
+            moving_on_fixed_image (np.ndarray): Moving image registered onto the fixed image.
         """
         moving_image_ants = ants.from_nibabel(moving_image)
         fixed_image_ants = ants.from_nibabel(fixed_image)
@@ -161,21 +171,24 @@ class ImageUtil():
 
         return moving_on_fixed_image
 
-
+# Can we use numba?
     def weighted_series_sum(self,
-                            image_series,
-                            image_meta,
-                            half_life: float) -> nibabel.nifti1:
+                            image_series: np.ndarray,
+                            image_meta: dict,
+                            half_life: float) -> np.ndarray:
         """
         Sum a 4D image series weighted based on time and re-corrected for decay correction.
 
         Args:
-            image_series (nibabel.nifti1): Input image to be summed.
-            image_meta (json file?): Metadata json file following BIDS standard, from which
+            image_series (np.ndarray): Input image to be summed.
+            image_meta (dict): Metadata json file following BIDS standard, from which
                                      we collect frame timing and decay correction info.
+            half_life (float): Half life of the PET radioisotope in seconds.
 
         Returns:
-            summed_image (nibabel.nifti1): Summed image 
+            summed_image (np.ndarray): Summed image 
+
+        Credit to Avi Snyder who wrote the original version of this code.
         """
         image_frame_start = image_meta['FrameTimesStart']
         image_frame_duration = image_meta['FrameDuration']
