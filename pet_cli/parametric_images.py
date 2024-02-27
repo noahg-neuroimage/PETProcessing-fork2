0cf9e38bae5289ea4e44df29433bef41eff638b9
Furqan Dar <darf@kenyon.edu>
2024-02-27 15:37:33 -0600

    Added docstring to parametric_images function
    This commit adds a detailed docstring to the function for generating parametric images in the pet_cli/parametric_images.py file. The docstring explains the purpose of the function, the arguments it requires, and the expected return. This enhances maintainability and clarity of the code for future developers.

--- pet_cli/parametric_images.py
+++ pet_cli/parametric_images.py
@@ -116,6 +116,39 @@ def generate_graphical_analysis_parametric_images_with_method(pTAC_times: np.nda
                                                               tTAC_img: np.ndarray,
                                                               t_thresh_in_mins: float,
                                                               analysis_func) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generates parametric images for 4D-PET data using the provided analysis method.
+
+    This function iterates over each voxel in the given `tTAC_img` and applies the provided `analysis_func`
+    to compute analysis values. The `analysis_func` should be a numba.jit function for optimization and
+    should be following a signature compatible with either of the following: patlak_analysis, logan_analysis,
+    or alt_logan_analysis.
+
+    Args:
+        pTAC_times (np.ndarray): A 1D array representing the input TAC times in minutes.
+        
+        pTAC_vals (np.ndarray): A 1D array representing the input TAC values. This array should
+                                be of the same length as `pTAC_times`.
+
+        tTAC_img (np.ndarray): A 4D array representing the 3D PET image over time.
+                               The shape of this array should be (x, y, z, time).
+
+        t_thresh_in_mins (float): A float representing the threshold time in minutes.
+                                  It is applied when calling the `analysis_func`.
+
+        analysis_func (Callable): A numba.jit function to apply to each voxel for given PET data.
+                                  It should take the following arguments:
+
+                                    - input_tac_values: 1D numpy array for input TAC values
+                                    - region_tac_values: 1D numpy array for regional TAC values
+                                    - tac_times_in_minutes: 1D numpy array for TAC times in minutes
+                                    - t_thresh_in_minutes: a float for threshold time in minutes
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple of two 3D numpy arrays representing the calculated slope image
+                                       and the intercept image, each of the same spatial dimensions as
+                                       `tTAC_img`.
+    """
     img_dims = tTAC_img.shape
     
     slope_img = np.zeros((img_dims[0], img_dims[1], img_dims[2]), float)
