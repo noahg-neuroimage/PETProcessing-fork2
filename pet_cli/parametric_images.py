54d23bafb02a30942f3b16e20fde244f005adb72
Furqan Dar <darf@kenyon.edu>
2024-02-29 16:36:27 -0600

    Add docstring to '_safe_load_tac' method
    Added a detailed doc string to the '_safe_load_tac' function in the parametric_images.py module. This docstring describes the function's purpose (to load time-activity curves from a file), its inputs, returns, and any errors that it might raise.

--- pet_cli/parametric_images.py
+++ pet_cli/parametric_images.py
@@ -114,6 +114,21 @@ def generate_parametric_images_with_graphical_method(pTAC_times: np.ndarray,
 
 
 def _safe_load_tac(filename: str) -> np.ndarray:
+    """
+    Loads time-activity curves (TAC) from a file.
+
+    Tries to read a TAC from specified file and raises an exception if unable to do so. We assume that the file has two
+    columns, the first corresponding to time and second corresponding to activity.
+
+    Args:
+        filename (str): The name of the file to be loaded.
+
+    Returns:
+        np.ndarray: A numpy array containing the loaded TAC. The first index corresponds to the times, and the second corresponds to the activity.
+
+    Raises:
+        Exception: An error occurred loading the TAC.
+    """
     try:
         return np.array(np.loadtxt(filename).T, dtype=float, order='C')
     except Exception as e:
