5c0ab009b16ad79632b95c1ddd5f1d6061e1dd0b
Furqan Dar <darf@kenyon.edu>
2024-03-13 14:01:56 -0500

    Enhance graphical plots with fitting information and additional control
    To provide more detailed graphical output, fitting parameters are now calculated in the 'calculate_x_and_y' method and stored. This change led to introducing a new method 'add_fit_points' that handles plotting of fitted points (in blue). Furthermore, 'add_plots' method has been added, offering fine-grained control over what aspects of the data to plot (data points, fit points, fit lines, shading). All these modifications aim to enhance visualization and data analysis in our toolkit.

--- pet_cli/graphical_plots.py
+++ pet_cli/graphical_plots.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from abc import ABC, abstractmethod
 import seaborn as sns
+from typing import Tuple, Dict
 from . import graphical_analysis as pet_grph
 
 
@@ -12,7 +13,7 @@ class GraphicalAnalysisPlot(ABC):
         self.tTAC = tTAC[:]
         self.t_thresh_in_mins = t_thresh_in_mins
         self.fig, self.ax_list = self.generate_figure_and_axes(figObj=figObj)
-        self.x, self.y = self.calculate_x_and_y()
+        self.x, self.y, self.fit_params = self.calculate_x_and_y()
         
     @staticmethod
     def generate_figure_and_axes(figObj: plt.Figure = None):
@@ -46,7 +47,30 @@ class GraphicalAnalysisPlot(ABC):
         for ax in self.ax_list:
             ax.plot(self.x[t_thresh:], self.y[t_thresh:], 'o', alpha=0.9, ms='5', zorder=2, color='blue')
     
+    def add_fit_lines(self):
+        y = self.x * self.fit_params['slope'] + self.fit_params['intercept']
+        for ax in self.ax_list:
+            ax.plot(self.x, y, '-', color='orange', lw=2.5, zorder=3, label=self.generate_label_from_fit_params())
+    
+    def add_plots(self, plot_data: bool = True,
+                        plot_fit_points: bool = True,
+                        plot_fit_lines: bool = True,
+                        fit_shading: bool = True):
+        for ax in self.ax_list:
+            if plot_data:
+                self.add_data_plots()
+            if plot_fit_points:
+                self.add_fit_points()
+            if plot_fit_lines:
+                self.add_fit_lines()
+            if fit_shading:
+                self.add_shading_plots()
+        
     
     @abstractmethod
-    def calculate_x_and_y(self):
+    def calculate_x_and_y(self) -> Tuple[np.ndarray, np.ndarray, Dict[str, float]]:
+        pass
+    
+    @abstractmethod
+    def generate_label_from_fit_params(self) -> str:
         pass
