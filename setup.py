1c7808804379addc5f61c9a0e875934506d317ad
B-Judge <bradley.a.judge@gmail.com>
2024-03-14 15:06:30 -0500

    Edited entry_points in setup.py for bids_cli testing.

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 setup(name='pet_cli', version='0.0.1', packages=find_packages(),
       install_requires=['numpy', 'scipy', 'numba', 'pandas', 'nibabel', 'antspyx', 'SimpleITK'],
-      entry_points={'console_scripts': ['pet-cli = pet_cli.cli:main', ], }, )
+      entry_points={'console_scripts': ['pet-cli = pet_cli.bids_cli:main', ], }, )
