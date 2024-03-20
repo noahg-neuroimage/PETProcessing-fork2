500e8b14f6d5193e38d7481d45c085eb8712848b
Furqan Dar <darf@kenyon.edu>
2024-03-20 12:19:07 -0500

    Changed the theme.

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@ language = 'English (US)'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'python_docs_theme'
-# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
 html_static_path = ['_static']
 
