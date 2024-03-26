c09408a0d7ee6b36cd2d8962ebf1dd4f2f79d92b
Furqan Dar <darf@kenyon.edu>
2024-03-26 10:07:33 -0500

    Added a caption to the TAC interpolation plot.

--- pet_cli/tac_interpolation.py
+++ pet_cli/tac_interpolation.py
@@ -8,44 +8,45 @@ The module comprises two classes: :class:`EvenlyInterpolate` and :class:`EvenlyI
 The :class:`EvenlyInterpolate` class takes in TAC times and values and a specified delta time, :math:`\\Delta t`, to
 resample data by interpolating TACs evenly with respect to time.
 
-The :class:`EvenlyInterpolateWithMax` class extends the functionality of `EvenlyInterpolate` by modifying the
+The :class:`EvenlyInterpolateWithMax` class extends the functionality of :class:`EvenlyInterpolate` by modifying the
 calculation of delta time, :math:`\\Delta t`, to explicitly sample the maximum value of the TAC.
 
 Example:
-
-In the proceeding example, we shall use the two classes to resample a fake TAC. For better visualization, we shall
-plot the resampled TACs shifted in the y-direction.
-    
-    .. plot::
-        :include-source:
+    In the proceeding example, we shall use the two classes to resample a fake TAC. For better visualization, we shall
+    plot the resampled TACs shifted in the y-direction.
         
-        import numpy as np
-        from pet_cli.tac_interpolation import EvenlyInterpolate, EvenlyInterpolateWithMax
-        import matplotlib.pyplot as plt
+        .. plot::
+            :include-source:
+            :caption: Evenly interpolating TACs can sometimes miss the maximum value of the provided TAC given the
+                time-step. Therefore, we can explicitly include the maximum value by modifying the time-step.
+            
+            import numpy as np
+            from pet_cli.tac_interpolation import EvenlyInterpolate, EvenlyInterpolateWithMax
+            import matplotlib.pyplot as plt
+            
+            # define some dummy TAC times and values
+            tac_times = np.array([0., 1., 2.5, 4.1, 7., 15.0])
+            tac_values = np.array([0., 0.8, 2., 1.5, 0.6, 0.0])
         
-        # define some dummy TAC times and values
-        tac_times = np.array([0., 1., 2.5, 4.1, 7., 15.0])
-        tac_values = np.array([0., 0.8, 2., 1.5, 0.6, 0.0])
-    
-        # instantiate EvenlyInterpolate object and resample TAC (and add shift for better visualization)
-        even_interp = EvenlyInterpolate(tac_times=tac_times, tac_values=tac_values+0.25, delta_time=1.0)
-        resampled_tac = even_interp.get_resampled_tac()
-    
-        # instantiate EvenlyInterpolateWithMax object and resample TAC (and add shift for better visualization)
-        even_interp_max = EvenlyInterpolateWithMax(tac_times=tac_times, tac_values=tac_values+0.5, samples_before_max=3)
-        resampled_tac_max = even_interp_max.get_resampled_tac()
+            # instantiate EvenlyInterpolate object and resample TAC (and add shift for better visualization)
+            even_interp = EvenlyInterpolate(tac_times=tac_times, tac_values=tac_values+0.25, delta_time=1.0)
+            resampled_tac = even_interp.get_resampled_tac()
         
-        # plot the TAC and the resampled TACs
-        fig, ax = plt.subplots(1,1, constrained_layout=True, figsize=(8,4))
-        plt.plot(tac_times, tac_values, 'ko--', label='Raw TAC', zorder=2)
-        plt.plot(*resampled_tac, 'ro-', label='Evenly Resampled TAC', zorder=1)
-        plt.plot(*resampled_tac_max, 'bo-', label='Evenly Resampled TAC w/ Max', zorder=0)
-        ax.text(s='resampled TACS are \\nshifted for visual clarity',
-                x=0.95, y=0.95, ha='right', va='top', transform=ax.transAxes, fontsize=16)
-        plt.xlabel('Time (s)', fontsize=16)
-        plt.ylabel('TAC Value', fontsize=16)
-        plt.legend(bbox_to_anchor=(1.0, 0.5), loc='center left')
-        plt.show()
+            # instantiate EvenlyInterpolateWithMax object and resample TAC (and add shift for better visualization)
+            even_interp_max = EvenlyInterpolateWithMax(tac_times=tac_times, tac_values=tac_values+0.5, samples_before_max=3)
+            resampled_tac_max = even_interp_max.get_resampled_tac()
+            
+            # plot the TAC and the resampled TACs
+            fig, ax = plt.subplots(1,1, constrained_layout=True, figsize=(8,4))
+            plt.plot(tac_times, tac_values, 'ko--', label='Raw TAC', zorder=2)
+            plt.plot(*resampled_tac, 'ro-', label='Evenly Resampled TAC', zorder=1)
+            plt.plot(*resampled_tac_max, 'bo-', label='Evenly Resampled TAC w/ Max', zorder=0)
+            ax.text(s='resampled TACS are \\nshifted for visual clarity',
+                    x=0.95, y=0.95, ha='right', va='top', transform=ax.transAxes, fontsize=16)
+            plt.xlabel('Time (s)', fontsize=16)
+            plt.ylabel('TAC Value', fontsize=16)
+            plt.legend(bbox_to_anchor=(1.0, 0.5), loc='center left')
+            plt.show()
         
 
 Note:
@@ -131,9 +132,9 @@ class EvenlyInterpolateWithMax(EvenlyInterpolate):
     def __init__(self, tac_times: np.ndarray, tac_values: np.ndarray, samples_before_max: float = 10.0):
         r"""Initializes an instance of the EvenlyInterpolateWithMax class for TAC interpolation.
 
-        This subclass of the EvenlyInterpolate class strives to ensure the maximum value of the Time-Activity Curve
-        (TAC) is included in the resampled TAC. It achieves this by determining a dynamic delta time value based on the
-        input TAC and the user-specified number of samples before the TAC's max value.
+        This subclass of the :class:EvenlyInterpolate class strives to ensure the maximum value of the Time Activity
+        Curve (TAC) is included in the resampled TAC. It achieves this by determining a dynamic delta time value based
+        on the input TAC and the user-specified number of samples before the TAC's max value.
 
         The constructor takes the TAC times and values as input, alongside a parameter that defines the number of
         samples to take before the max value of the TAC. It computes the delta time using a static method of the class,
@@ -160,9 +161,8 @@ class EvenlyInterpolateWithMax(EvenlyInterpolate):
         while preserving the maximum value of the TAC in the sample.
 
         This method determines the time-step so that when the TAC gets resampled,
-        it is guaranteed that the maximum value of the TAC in the input is also sampled in the resample.
-
-        The :math:`\Delta t` is calculated based on the equation:
+        it is guaranteed that the maximum value of the TAC in the input is also sampled in the resample. The
+        :math:`\Delta t` is calculated based on the equation:
         
         .. math::
         
@@ -179,8 +179,8 @@ class EvenlyInterpolateWithMax(EvenlyInterpolate):
             samples_before_max (float): The number of samples that you want before the maximum TAC value is reached.
 
         Returns:
-            float: The calculated time-step or :math:`\Delta t` that would ensure that the TAC is evenly sampled,
-                and the maximum TAC value is included in the samples.
+            (float): The calculated time-step, :math:`\Delta t`, that ensures that the TAC is evenly sampled, and the
+            maximum TAC value is included in the samples.
         """
         t_start = tac_times[0]
         t_for_max_val = tac_times[np.argmax(tac_values)]
