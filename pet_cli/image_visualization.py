af88510e65e4a0d4d3011ce84730451bd2c688c1
Furqan Dar <darf@kenyon.edu>
2024-03-26 16:29:47 -0500

    Update image loader and add frame methods in ImageVisualization
    Modified image loading in ImageVisualization to use a safer method. Removed the initial assignment of self.ani_image and added the make_first_frame and update_frame methods. These changes provide functionality to generate the first frame and update subsequent frames of the visualization for different axes views.

--- pet_cli/image_visualization.py
+++ pet_cli/image_visualization.py
@@ -25,8 +25,6 @@ class NiftiGifCreator:
         
         self.vmax = max(np.max(self.image), np.abs(np.min(self.image)))
         
-        self.ani_image = None
-        
         self.fig, self.ax = plt.subplots(1, 1, constrained_layout=True)
         
         self.imKW = {'origin': 'lower',
@@ -34,8 +32,40 @@ class NiftiGifCreator:
                      'vmin': -self.vmax / 3.,
                      'vmax': self.vmax / 3.,
                      'interpolation': 'none'}
+        
+        self.ani_image = self.make_first_frame(axis=self.view)
     
     def _validate_view(self):
         if self.view not in ['coronal', 'sagittal', 'axial', 'x', 'y', 'z']:
             raise ValueError("Invalid view. Please choose from 'coronal', 'sagittal', 'axial', 'x', 'y', 'z'.")
+    
+    def make_first_frame(self, axis):
+        if axis in ['x', 'coronal']:
+            img = self.image[0, :, :].T
+        elif axis == ['y', 'sagittal']:
+            img = self.image[:, 0, :].T
+        else:
+            img = self.image[:, :, 0].T
+        out_im = self.ax.imshow(img, **self.imKW)
+        
+        cbar = self.fig.colorbar(out_im, ax=self.ax, shrink=1.0)
+        cbar.set_label('$K_i$ (Infusion Rate)', rotation=270)
+        self.fig.suptitle("Patlak-$K_i$ Parametric Image")
+        out_im.axes.get_xaxis().set_visible(False)
+        out_im.axes.get_yaxis().set_visible(False)
+        
+        return out_im
+        
+        
+    def update_frame(self, i, axis):
+        if axis in ['x', 'coronal']:
+            img = self.image[i, :, :].T
+        elif axis == ['y', 'sagittal']:
+            img = self.image[:, i, :].T
+        else:
+            img = self.image[:, :, i].T
+        
+        self.ani_image.set_data(img)
+        
+        return self.ani_image,
         
