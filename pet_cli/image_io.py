c67f4908c54a4f425931ef26538a9232d5b1d5a6
Furqan Dar <darf@kenyon.edu>
2024-03-29 14:50:05 -0500

    Improve spacing and clean up code in image_io.py module
    Standardized code formatting for improved readability in the image_io.py module. This includes fixing spacing around commas and operators, removing unnecessary blank lines, and simplifying complex expressions. The save_nii() method's return type hint has been removed as well. Changed read_ctab to read_color_table_json, and now it also correctly opens a file and loads using json.

--- pet_cli/image_io.py
+++ pet_cli/image_io.py
@@ -69,13 +69,13 @@ class ImageIO():
             raise OSError(f"{image_path} does not have the extension .nii or .nii.gz")
         
         image = nibabel.load(image_path)
-
+        
         if self.verbose:
             print(f"(ImageIO): {image_path} loaded")
-
+        
         return image
     
-    def save_nii(self, image: nibabel.nifti1.Nifti1Image, out_file: str) -> int:
+    def save_nii(self, image: nibabel.nifti1.Nifti1Image, out_file: str):
         """
         Wrapper to save nifti to file.
 
@@ -83,11 +83,10 @@ class ImageIO():
             image (nibabel.nifti1.Nifti1Image): Nibabel-type image to write to file.
             out_file (str): File path to which image will be written.
         """
-        nibabel.save(image,out_file)
+        nibabel.save(image, out_file)
         if self.verbose:
             print(f"(ImageIO): Image saved to {out_file}")
-
-
+    
     def extract_image_from_nii_as_numpy(self, image: nibabel.nifti1.Nifti1Image) -> np.ndarray:
         """
         Convenient wrapper to extract data from a .nii or .nii.gz file as a numpy array.
@@ -99,13 +98,12 @@ class ImageIO():
             The data contained in the .nii or .nii.gz file as a numpy array.
         """
         image_data = image.get_fdata()
-
+        
         if self.verbose:
             print(f"(ImageIO): Image has shape {image_data.shape}")
-
+        
         return image_data
-
-
+    
     def extract_header_from_nii(self, image: nibabel.nifti1.Nifti1Image) -> FileBasedHeader:
         """
         Convenient wrapper to extract header information from a .nii or .nii.gz 
@@ -118,13 +116,12 @@ class ImageIO():
             image_header (FileBasedHeader): The nifti header.
         """
         image_header = image.header
-
+        
         if self.verbose:
             print(f"(ImageIO): Image header is: {image_header}")
-
+        
         return image_header
-
-
+    
     def extract_np_to_nibabel(self,
                               image_array: np.ndarray,
                               header: FileBasedHeader,
@@ -140,9 +137,9 @@ class ImageIO():
         Returns:
             image_nibabel (nibabel.nifti1.Nifti1Image): Image stored in nifti-like nibabel format. 
         """
-        image_nibabel = nibabel.nifti1.Nifti1Image(image_array,affine,header)
+        image_nibabel = nibabel.nifti1.Nifti1Image(image_array, affine, header)
         return image_nibabel
-
+    
     @staticmethod
     def affine_parse(image_affine: np.ndarray) -> tuple:
         """
@@ -153,20 +150,17 @@ class ImageIO():
                 and direction of an image.
         """
         spacing = nibabel.affines.voxel_sizes(image_affine)
-        origin = image_affine[:,3]
-
-        quat = nibabel.quaternions.mat2quat(image_affine[:3,:3])
+        origin = image_affine[:, 3]
+        
+        quat = nibabel.quaternions.mat2quat(image_affine[:3, :3])
         dir_3x3 = nibabel.quaternions.quat2mat(quat)
-        direction = np.zeros((4,4))
-        direction[-1,-1] = 1
-        direction[:3,:3] = dir_3x3
-
+        direction = np.zeros((4, 4))
+        direction[-1, -1] = 1
+        direction[:3, :3] = dir_3x3
+        
         return spacing, origin, direction
-
-
-    def extract_np_to_ants(self,
-                           image_array: np.ndarray,
-                           affine: np.ndarray) -> ants.ANTsImage:
+    
+    def extract_np_to_ants(self, image_array: np.ndarray, affine: np.ndarray) -> ants.ANTsImage:
         """
         Wrapper to convert an image array into ants object.
         Note header info is lost as ANTs does not carry this metadata.
@@ -179,14 +173,11 @@ class ImageIO():
             image_ants (ants.ANTsImage): Image stored in nifti-like nibabel format. 
         """
         origin, spacing, direction = self.affine_parse(affine)
-        image_ants = ants.from_numpy(data=image_array,
-                                     spacing=spacing,
-                                     origin=origin,
-                                     direction=direction)
+        image_ants = ants.from_numpy(data=image_array, spacing=spacing, origin=origin, direction=direction)
         return image_ants
-
+    
     @staticmethod
-    def read_ctab(ctab_file: str) -> dict:
+    def read_color_table_json(ctab_file: str) -> dict:
         """
         Static method to read a color table, translating region indices to region names, 
         as a dictionary. Assumes json format.
@@ -202,9 +193,10 @@ class ImageIO():
         """
         if not os.path.exists(ctab_file):
             raise FileNotFoundError(f"Image file {ctab_file} not found")
-        ctab_json = json.load(ctab_file)
+        with open(ctab_file, "r", encoding="utf-8") as c_file:
+            ctab_json = json.load(c_file)
         return ctab_json
-
+    
     @staticmethod
     def load_metadata_for_nifty_with_same_filename(image_path) -> dict:
         """
