f3013559db5ad63d4885598dfe49386268d07579
Furqan Dar <darf@kenyon.edu>
2024-04-02 16:15:53 -0500

    Refactor command-line interface in PET preprocessing script
    Refactored the command-line interface in the PET preprocessing script to use sub-commands instead of an 'operation' argument. This change makes the command-line help more intuitive and specific to each command. The modification also included necessary adjustments in the associated documentation and examples.

--- pet_cli/cli_preproc.py
+++ pet_cli/cli_preproc.py
@@ -3,6 +3,7 @@ This module provides a Command-line interface (CLI) for preprocessing imaging da
 produce regional PET Time-Activity Curves (TACs) and prepare data for parametric imaging analysis.
 
 The user must provide:
+    * The sub-command. Options: 'weighted_sum', 'motion_correct', 'register', or 'write_tacs'.
     * Path to PET input data in NIfTI format. This can be source data, or with some preprocessing
       such as registration or motion correction, depending on the chosen operation.
     * Directory to which the output is written.
@@ -10,33 +11,32 @@ The user must provide:
     * 3D imaging data, such as anatomical, segmentation, or PET sum, depending on the desired
       preprocessing operation.
     * Additional information needed for preprocessing, such as color table or half-life.
-    * The operation to be performed on input data. Options: 'weighted_sum', 'motion_correct',
-      'register', or 'write_tacs'.
+    
 
 Examples:
     * Half-life Weighted Sum:
     
         .. code-block:: bash
     
-            pet-cli-preproc --operation weighted_sum --pet /path/to/pet.nii --out_dir /path/to/output
+            pet-cli-preproc weighted_sum --pet /path/to/pet.nii --out_dir /path/to/output
     
     * Image Registration:
     
         .. code-block:: bash
     
-            pet-cli-preproc --operation register --pet /path/to/pet.nii --anatomical /path/to/mri.nii --pet_reference /path/to/pet_sum.nii --out_dir /path/to/output
+            pet-cli-preproc register --pet /path/to/pet.nii --anatomical /path/to/mri.nii --pet_reference /path/to/pet_sum.nii --out_dir /path/to/output
             
     * Motion Correction:
     
         .. code-block:: bash
             
-            pet-cli-preproc --operation motion_correct --pet /path/to/pet.nii --pet-reference /path/to/sum.nii --out_dir /path/to/output
+            pet-cli-preproc motion_correct --pet /path/to/pet.nii --pet-reference /path/to/sum.nii --out_dir /path/to/output
             
     * Extracting TACs Using A Mask And Color-Table:
     
         .. code-block:: bash
             
-            pet-cli-preproc --operation write_tacs --pet /path/to/pet.nii --segmentation /path/to/seg_masks.nii --color_table_path /path/to/color_table.json --out_dir /path/to/output
+            pet-cli-preproc write_tacs --pet /path/to/pet.nii --segmentation /path/to/seg_masks.nii --color_table_path /path/to/color_table.json --out_dir /path/to/output
 
 See Also:
     * :mod:`pet_cli.image_operations_4d` - module used to preprocess PET imaging data.
@@ -87,13 +87,13 @@ def _generate_image_path_and_directory(main_dir, ops_dir_name, file_prefix, ops_
 _PREPROC_EXAMPLES_ = (r"""
 Examples:
   - Weighted Sum:
-    pet-cli-preproc --operation weighted_sum --pet /path/to/pet.nii --out_dir /path/to/output
+    pet-cli-preproc weighted_sum --pet /path/to/pet.nii --out_dir /path/to/output
   - Registration:
-    pet-cli-preproc --operation register --pet /path/to/pet.nii --anatomical /path/to/mri.nii --pet_reference /path/to/pet_sum.nii --out_dir /path/to/output
+    pet-cli-preproc register --pet /path/to/pet.nii --anatomical /path/to/mri.nii --pet_reference /path/to/pet_sum.nii --out_dir /path/to/output
   - Motion Correction:
-    pet-cli-preproc --operation motion_correct --pet /path/to/pet.nii --pet-reference /path/to/sum.nii --out_dir /path/to/output
+    pet-cli-preproc motion_correct --pet /path/to/pet.nii --pet-reference /path/to/sum.nii --out_dir /path/to/output
   - Writing TACs From Segmentation Masks:
-    pet-cli-preproc --operation write_tacs --pet /path/to/pet.nii --segmentation /path/to/seg_masks.nii --color_table_path /path/to/color_table.json --out_dir /path/to/output
+    pet-cli-preproc write_tacs --pet /path/to/pet.nii --segmentation /path/to/seg_masks.nii --color_table_path /path/to/color_table.json --out_dir /path/to/output
 """)
 
 
@@ -105,23 +105,38 @@ def _generate_args() -> argparse.Namespace:
         args (argparse.Namespace): Arguments used in the command line and their corresponding values.
     """
     parser = argparse.ArgumentParser(prog='PET Preprocessing',
-                                     description='Command line interface for running PET preprocessing steps.',
+                                     description='Command line interface for running PET pre-processing steps.',
                                      epilog=_PREPROC_EXAMPLES_, formatter_class=argparse.RawTextHelpFormatter)
-    io_grp = parser.add_argument_group('I/O')
-    io_grp.add_argument('--pet', required=True, help='Path to PET series')
-    io_grp.add_argument('--anatomical', required=False, help='Path to 3D anatomical image')
-    io_grp.add_argument('--segmentation', required=False, help='Path to segmentation image\
-        in anatomical space')
-    io_grp.add_argument('--pet_reference', required=False, help='Path to reference image\
-        for motion correction, if not weighted_sum.')
-    io_grp.add_argument('--color_table_path', required=False, help='Path to color table')
-    io_grp.add_argument('--half_life', required=False, help='Half life of radioisotope in seconds.', type=float)
-    io_grp.add_argument('--out_dir', required=True, help='Directory to write results to')
-    io_grp.add_argument('--subject_id', required=False, help='Subject ID to name files with', default='sub')
     
-    ops_grp = parser.add_argument_group('Operations')
-    ops_grp.add_argument('--operation', required=True, help='Preprocessing operation to perform',
-                         choices=['weighted_sum', 'motion_correct', 'register', 'write_tacs'])
+    parser.add_argument('-p', '--pet', required=True, help='Path to PET series.', type=str)
+    parser.add_argument('-o', '--out_dir', default="./", help='Output directory path.', type=str)
+    parser.add_argument('-f', '--prefix', default="sub-XXXX", help='Filename prefix for output files.', type=str)
+    
+    # create subparsers
+    subparsers = parser.add_subparsers(dest="command")
+    
+    # create parser for "weighted_sum" command
+    parser_sum = subparsers.add_parser('weighted_sum')
+    parser_sum.add_argument('-h', '--half-life', required=True, help='Half life of radioisotope in seconds.',
+                            type=float)
+    
+    # create parser for "register" command
+    parser_reg = subparsers.add_parser('register')
+    parser_reg.add_argument('-a', '--anatomical', required=True, help='Path to 3D anatomical image (T1w or T2w).',
+                            type=str)
+    
+    # create parser for the "motion_correct" command
+    parser_moco = subparsers.add_parser('motion_correct')
+    parser_moco.add_argument('-r', '--pet-reference', default=None,
+                             help='Path to reference image for motion correction, if not weighted_sum.')
+    
+    # create parser for the "write_tacs" command
+    parser_tac = subparsers.add_parser('write_tacs')
+    parser_tac.add_argument('-s', '--segmentation', required=True,
+                            help='Path to segmentation image in anatomical space.')
+    parser_tac.add_argument('-c', '--color_table_path', required=True, help='Path to color table in JSON format')
+    parser_tac.add_argument('-r', '--resample_segmentation', default=True, action='store_true',
+                            help='Resample segmentation.')
     
     verb_group = parser.add_argument_group('Additional information')
     verb_group.add_argument('-v', '--verbose', action='store_true',
