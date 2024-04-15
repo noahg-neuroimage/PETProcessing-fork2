8cf50f59a4e706594a92ec1c4bf16603c35f9999
Furqan Dar <darf@kenyon.edu>
2024-04-15 11:10:18 -0500

    Implemented a function that returns the TAC (with FRTM) given simplified parameters and a reference TAC. Based on http://www.turkupetcentre.net/petanalysis/model_compartmental_ref.html

--- pet_cli/reference_tissue_models.py
+++ pet_cli/reference_tissue_models.py
@@ -11,3 +11,18 @@ def calc_srtm_tac(tac_times: np.ndarray, r1: float, k2: float, bp: float, ref_ta
     second_term = (k2 - r1 * bp_coeff) * tcms_conv.calc_convolution_with_check(f=exp_term, g=ref_tac_vals, dt=dt)
     
     return first_term + second_term
+
+
+def calc_frtm_tac(tac_times: np.ndarray,
+                  r1: float,
+                  a1: float,
+                  a2: float,
+                  alpha_1: float,
+                  alpha_2: float,
+                  ref_tac_vals: np.ndarray) -> np.ndarray:
+    first_term = r1 * ref_tac_vals
+    exp_funcs = a1 * np.exp(-alpha_1 * tac_times) + a2 * np.exp(-alpha_2 * tac_times)
+    dt = tac_times[1] - tac_times[0]
+    second_term = tcms_conv.calc_convolution_with_check(f=exp_funcs, g=ref_tac_vals, dt=dt)
+    return first_term + second_term
+
