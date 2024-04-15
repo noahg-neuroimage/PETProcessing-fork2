e35b30a87fe6f3a83c787c9237edb631747b00d2
Furqan Dar <darf@kenyon.edu>
2024-04-15 11:15:41 -0500

    Implemented a function for calculating the function parameters for the FRTM given kinetic parameters.

--- pet_cli/reference_tissue_models.py
+++ pet_cli/reference_tissue_models.py
@@ -26,3 +26,15 @@ def calc_frtm_tac(tac_times: np.ndarray,
     second_term = tcms_conv.calc_convolution_with_check(f=exp_funcs, g=ref_tac_vals, dt=dt)
     return first_term + second_term
 
+
+def calc_frtm_params_from_kinetic_params(r1: float,
+                                         k2: float,
+                                         k3: float,
+                                         k4: float) -> tuple[float, float, float, float, float]:
+    beta = k2 + k3 + k4
+    chi = np.sqrt(beta ** 2. - 4.0 * k2 * k4)
+    alpha_1 = (beta - chi) / 2.0
+    alpha_2 = (beta + chi) / 2.0
+    a1 = (k3 + k4 - alpha_2) / chi * (k2 / r1 - alpha_2)
+    a2 = (alpha_1 - k3 - k4) / chi * (k2 / r1 - alpha_1)
+    return r1, a1, a2, alpha_1, alpha_2
