35e3a21dc1633cf5d76d0112591631516399ea06
B-Judge <bradley.a.judge@gmail.com>
2024-04-16 20:01:16 -0500

    Added edits to Karl's "simple" method over from Jupyter notebook. Added autogenerated docstrings.

--- pet_cli/image_derived_input_function.py
+++ pet_cli/image_derived_input_function.py
@@ -1,101 +1,73 @@
 import numpy as np
 
 
-def make_early_mean_image_from_4d_pet(pet_4d_data: np.ndarray,
-                                      start_frame: int = 3,
-                                      end_frame: int = 7) -> np.ndarray:
+def karl_simple_karl_idif_from_4d_pet_with_necktangle(pet_4d_data: np.ndarray,
+                                                      carotid_necktangle_mask_3d_data: np.ndarray,
+                                                      percentile: float,
+                                                      bolus_start_frame: int = 3,
+                                                      bolus_end_frame: int = 7) -> np.ndarray:
     """
-    Calculate the mean image across a specified range of frames in a 4D PET data array.
+    Applies the Karl simple percentile IDIF method to calculate the time-activity curve (TAC) from PET data.
 
     Args:
-        pet_4d_data (np.ndarray): A 4D numpy array representing PET data over time, with shape (time_frames, height, width, depth).
-        start_frame (int): The starting frame index (inclusive). Defaults to 3.
-        end_frame (int): The ending frame index (inclusive). Defaults to 7.
+        pet_4d_data (np.ndarray): 4-dimensional array representing the PET data.
+        carotid_necktangle_mask_3d_data (np.ndarray): 3-dimensional array representing the carotid mask data.
+        percentile (float): The percentile used as the threshold value for masking the data.
+        bolus_start_frame (int, optional): The starting frame index for the bolus mean calculation. Defaults to 3.
+        bolus_end_frame (int, optional): The ending frame index for the bolus mean calculation. Defaults to 7.
 
     Returns:
-        np.ndarray: A 3D numpy array representing the mean image across the specified frames.
-
-    Raises:
-        ValueError: If the start or end frame indices are out of the array's bounds.
+        tac (np.ndarray): The time-activity curve (TAC) calculated using the Karl simple percentile IDIF method.
     """
-    if start_frame < 0 or end_frame >= pet_4d_data.shape[0]:
-        raise ValueError("Frame indices are out of bounds.")
+    bolus_mean_3d = average_across_4d_frames(pet_4d_data=pet_4d_data,
+                                             start_frame=bolus_start_frame,
+                                             end_frame=bolus_end_frame)
+    print(np.nanmean(bolus_mean_3d))
+    print(np.nanmean(carotid_necktangle_mask_3d_data))
 
-    early_mean = np.mean(pet_4d_data[start_frame:end_frame + 1], axis=0)
-
-    return early_mean
-
-
-def crop_by_input_function_mask(early_mean_data: np.ndarray,
-                                carotid_mask_data: np.ndarray) -> np.ndarray:
-    """
-    Apply a binary mask to an early mean image, effectively cropping or masking the image.
-
-    Args:
-        early_mean_data (np.ndarray): The early mean image data as a 3D numpy array.
-        carotid_mask_data (np.ndarray): A binary mask data as a 3D numpy array of the same shape as early_mean_data.
-
-    Returns:
-        np.ndarray: The masked image data, retaining only the parts of early_mean_data where carotid_mask_data is not zero.
-
-    Raises:
-        ValueError: If early_mean_data and carotid_mask_data do not have the same shape.
-    """
-    if early_mean_data.shape != carotid_mask_data.shape:
+    if bolus_mean_3d.shape != carotid_necktangle_mask_3d_data.shape:
         raise ValueError("array1 and array2 must have the same shape.")
 
-    masked_data = early_mean_data * carotid_mask_data
-    return masked_data
+    carotid_masked_bolus_mean_3d = np.where(carotid_necktangle_mask_3d_data == 1, bolus_mean_3d, np.nan)
+    print(np.nanmean(carotid_masked_bolus_mean_3d))
+
+    threshold_value = np.nanpercentile(carotid_masked_bolus_mean_3d, percentile)
+    print(threshold_value)
+    bolus_mean_threshold_masked_3d = np.where(carotid_masked_bolus_mean_3d > threshold_value,
+                                              carotid_masked_bolus_mean_3d, np.nan)
+    bolus_mean_threshold_binary_mask_3d = np.where(bolus_mean_threshold_masked_3d >= 1000, 1, 0)
+
+    pet_data_4d_masked = np.zeros(pet_4d_data.shape)
+    for frame in range(pet_data_4d_masked.shape[3]):
+        pet_data_4d_masked[:, :, :, frame] = np.where(bolus_mean_threshold_binary_mask_3d == 1,
+                                                      pet_4d_data[:, :, :, frame], np.nan)
+
+    tac = np.nanmean(pet_data_4d_masked, axis=(0, 1, 2))
+
+    return tac
 
 
-def make_threshold_binary_mask(masked_data: np.ndarray,
-                               threshold: float) -> np.ndarray:
+def average_across_4d_frames(pet_4d_data: np.ndarray,
+                             start_frame: int,
+                             end_frame: int) -> np.ndarray:
     """
-    Create a binary mask based on a threshold, setting values below the threshold to 0 and values at or above to 1.
+    Calculates the mean frame across the specified range of frames in the 4-dimensional PET data.
 
     Args:
-        masked_data (np.ndarray): The image data to threshold as a numpy array.
-        threshold (float): The threshold value.
+        pet_4d_data (np.ndarray): 4-dimensional array representing the PET data.
+        start_frame (int): The starting frame index for the mean calculation.
+        end_frame (int): The ending frame index for the mean calculation.
 
     Returns:
-        np.ndarray: A binary mask of the same shape as masked_data.
+        mean_frame (np.ndarray): The mean frame calculated from the specified range of frames.
     """
-    threshold_binary_data = np.where(masked_data < threshold, 0, 1)
-    return threshold_binary_data
+    if start_frame < 0 or end_frame >= pet_4d_data.shape[3]:
+        raise ValueError("Frame indices are out of bounds.")
 
+    mean_frame = np.nanmean(pet_4d_data[:, :, :, start_frame:end_frame + 1], axis=3)
 
-def apply_threshold_binary_mask_to_4d_pet(pet_4d_data: np.ndarray,
-                                          threshold_binary_data: np.ndarray) -> np.ndarray:
-    """
-    Apply a binary mask to each frame of a 4D PET data array.
+    return mean_frame
 
-    Args:
-        pet_4d_data (np.ndarray): The 4D PET data array with shape (time_frames, height, width, depth).
-        threshold_binary_data (np.ndarray): A binary mask with the same spatial dimensions as the frames in pet_4d_data.
-
-    Returns:
-        np.ndarray: The masked 4D PET data.
-    """
-    masked_4d_pet = pet_4d_data * threshold_binary_data
-    return masked_4d_pet
-
-
-def average_masked_4d_pet_into_tac(masked_4d_pet_data: np.ndarray) -> np.ndarray:
-    """
-    Average the values of each 3D frame in a 4D masked PET data array into a 1D numpy array of the averages.
-
-    Args:
-        masked_4d_pet_data (np.ndarray): The 4D masked PET data array with shape (time_frames, height, width, depth).
-
-    Returns:
-        np.ndarray: A 1D numpy array where each element represents the average of the corresponding 3D frame in the masked 4D PET data.
-    """
-    frame_averages = np.mean(masked_4d_pet_data, axis=(1, 2, 3))
-    return frame_averages
-
-
-# Below is longer methods
-# -----------------------------------------------------------------
 
 def get_frame_time_midpoints(frame_start_times: np.ndarray,
                              frame_duration_times: np.ndarray) -> np.ndarray:
@@ -147,9 +119,9 @@ def load_fslmeants_to_numpy_3d(fslmeants_filepath: str) -> np.ndarray:
     return numpy_3d_array
 
 
-def get_idif_from_4d_pet_necktangle(necktangle_matrix: np.ndarray,
-                                    percentile: float,
-                                    frame_midpoint_times: np.ndarray) -> np.ndarray:
+def karl_complex_idif_from_4d_pet_necktangle(necktangle_matrix: np.ndarray,
+                                             percentile: float,
+                                             frame_midpoint_times: np.ndarray) -> np.ndarray:
     """
     Computes the IDIF from a 4D PET necktangle matrix given a percentile for thresholding.
     This function finds the highest mean frame from the first 10 frames of the 4D PET, creates a mean 3D image of that frame, the one before it, and the one after it.
