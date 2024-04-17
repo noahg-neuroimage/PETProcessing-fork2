3a119e91690eea74bee2bff2617bbccccb1c9045
B-Judge <bradley.a.judge@gmail.com>
2024-04-16 19:06:15 -0500

    Edited variable names, added docstrings to the second method code.

--- pet_cli/image_derived_input_function.py
+++ pet_cli/image_derived_input_function.py
@@ -101,33 +101,35 @@ def average_masked_4d_pet_into_tac(masked_4d_pet_data: np.ndarray) -> np.ndarray
 def get_frame_time_midpoints(frame_start_times: np.ndarray,
                              frame_duration_times: np.ndarray) -> np.ndarray:
     """
-    Calculate the midpoints of frame times given the start times and durations.
+        Calculates the midpoint times of each frame based on the start times and duration times.
+
+        Args:
+            frame_start_times (np.ndarray): An array of frame start times.
+            frame_duration_times (np.ndarray): An array of frame duration times.
+
+        Returns:
+            frame_midpoint_times (np.ndarray): An array of frame midpoint times.
+
+        Raises:
+            None.
+        """
+    frame_midpoint_times = (frame_start_times + (frame_duration_times / 2)).astype(int)
+    return frame_midpoint_times
+
+
+def load_fslmeants_to_numpy_3d(fslmeants_filepath: str) -> np.ndarray:
+    """
+    Loads `fslmeants <https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/PPIHowToRun?highlight=%28fslmeants%29>`_ (--show-all) data from a file and converts it into a 3D numpy array.
 
     Args:
-        frame_start_times (np.ndarray): An array of frame start times.
-        frame_duration_times (np.ndarray): An array of durations corresponding to each start time.
+        fslmeants_filepath (str): The filepath of the fslmeants data file.
 
     Returns:
-        np.ndarray: An array of midpoint times calculated as the start time plus half of the duration for each frame.
+        numpy_3d_array (np.ndarray): A 3D numpy array representing the fslmeants data.
 
+    Raises:
+        None.
     """
-    frame_midpoint_times = frame_start_times + (frame_duration_times / 2)
-    return frame_midpoint_times.astype(int)
-
-
-def load_fslmeants_to_numpy(fslmeants_filepath: str) -> np.ndarray:
-    """
-    Load the fslmeants output file from a CSV and convert it to a NumPy array, removing the first three rows
-    which are assumed to be coordinate data.
-
-    Args:
-        fslmeants_filepath (str): The file path to the CSV containing the fslmeants output.
-
-    Returns:
-        np.ndarray: A 2D NumPy array where each row corresponds to a time point and each column to a different ROI.
-
-    """
-    # data = pd.read_csv(fslmeants_filepath, delim_whitespace=True, header=None)
     data = np.loadtxt(fslmeants_filepath)
     x_coord_min = min(data[0].astype(int))
     y_coord_min = min(data[1].astype(int))
@@ -136,35 +138,35 @@ def load_fslmeants_to_numpy(fslmeants_filepath: str) -> np.ndarray:
     y_dim = (max(data[1].astype(int)) - y_coord_min) + 1
     z_dim = (max(data[2].astype(int)) - z_coord_min) + 1
     t_dim = data.shape[0] - 3
-    necktangled_matrix = np.zeros((x_dim, y_dim, z_dim, t_dim), dtype=float)
+    numpy_3d_array = np.zeros((x_dim, y_dim, z_dim, t_dim), dtype=float)
     for location in range(data.shape[1]):
         x_coord = data[0, location].astype(int) - x_coord_min
         y_coord = data[1, location].astype(int) - y_coord_min
         z_coord = data[2, location].astype(int) - z_coord_min
-        necktangled_matrix[x_coord, y_coord, z_coord, :] = data[3:, location]
+        numpy_3d_array[x_coord, y_coord, z_coord, :] = data[3:, location]
 
-    return necktangled_matrix
+    return numpy_3d_array
 
 
 def get_idif_from_4d_pet_necktangle(necktangle_matrix: np.ndarray,
                                     percentile: float,
                                     frame_midpoint_times: np.ndarray) -> np.ndarray:
     """
-    Process the fslmeants data to identify the bolus frame based on the highest mean value within the first ten frames,
-    determine 'carotid' voxels based on a percentile cut-off around the bolus frame, and calculate a specified percentile
-    for these voxels across all frames.
+    Computes the IDIF from a 4D PET necktangle matrix given a percentile for thresholding.
+    This function finds the highest mean frame from the first 10 frames of the 4D PET, creates a mean 3D image of that frame, the one before it, and the one after it.
+    Then, this function applies an automatic percentile thresholding of 90% to that mean image to generate a carotid mask.
+    Finally, that carotid mask is applied to the 4D PET image, and the resulting 4D image undergoes percentile thresholding of the "percentile" value frame by frame to get the TAC.
 
     Args:
-        fslmeants_vals (np.ndarray): The NumPy array of fslmeants values, where each row is a time point and each column is an ROI.
-        percentile (float): The percentile threshold used to identify 'carotid' voxels.
-        frame_midpoint_times (np.ndarray): An array of frame times to be associated with each percentile value calculated.
+        necktangle_matrix (np.ndarray): A 4D numpy array representing the PET necktangle matrix.
+        percentile (float): The percentile value to calculate the manual threshold.
+        frame_midpoint_times (np.ndarray): An array of frame midpoint times.
 
     Returns:
-        np.ndarray: A 2D array with each row containing a frame time and the corresponding percentile value for the 'carotid' voxels.
+        tac (np.ndarray): A 2D numpy array representing the time-activity curve (TAC) with frame midpoint times and manual thresholds.
 
     Raises:
-        ValueError: If the length of frame_midpoint_times is less than the number of percentile values calculated.
-
+        None.
     """
     first_ten_frames = necktangle_matrix[:, :, :, :10]
     frame_averages = np.nanmean(first_ten_frames, axis=(0, 1, 2))
