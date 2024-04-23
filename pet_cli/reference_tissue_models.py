93b292d58e07de1ae082d484bfe0c5051df5921e
Furqan Dar <darf@kenyon.edu>
2024-04-23 10:51:38 -0500

    Added a detailed docstring for the bounded SRTM fitting function.

--- pet_cli/reference_tissue_models.py
+++ pet_cli/reference_tissue_models.py
@@ -235,7 +235,7 @@ def fit_srtm_model_to_tac(tgt_tac_vals: np.ndarray,
         bp_start (float): Starting guess for the binding potential.
 
     Returns:
-        tuple: (fit_parameters, fit_covariance). Output from :func:`scipy.optimize.curve_fit`
+        tuple: (``fit_parameters``, ``fit_covariance``). Output from :func:`scipy.optimize.curve_fit`
         
     Raises:
         AssertionError: If the reference TAC and times are different dimensions.
@@ -254,6 +254,38 @@ def fit_srtm_model_to_tac_with_bounds(tgt_tac_vals: np.ndarray,
                                       r1_bounds: np.ndarray = np.asarray([0.5, 0.0, 10.0]),
                                       k2_bounds: np.ndarray = np.asarray([0.5, 0.0, 10.0]),
                                       bp_bounds: np.ndarray = np.asarray([0.5, 0.0, 10.0])) -> tuple:
+    r"""
+    Fit SRTM to the provided target Time Activity Curve (TAC), given the reference TAC, times, and bounds for
+    the kinetic parameters.
+    
+    .. important::
+        This function assumes that the reference TAC is uniformly sampled with respect to time since we perform
+        convolutions.
+
+    This function is a wrapper around `scipy.optimize.curve_fit` and uses parameter bounds during optimization. The
+    bounds for each parameter are formatted as: ``(starting_value, lo_bound, hi_bound)``.
+
+    Args:
+        tgt_tac_vals (np.ndarray): Target TAC to fit with the SRTM.
+        ref_tac_times (np.ndarray): Times of the reference TAC data.
+        ref_tac_vals (np.ndarray): Reference TAC values.
+        r1_bounds (np.ndarray): The bounds for the :math:`R_1\equiv\frac{k_1^\prime}{k_1}` parameter.
+        Defaults to [0.5, 0.0, 10.0].
+        k2_bounds (np.ndarray): The bounds for :math:`k_2` parameter. Defaults to [0.5, 0.0, 10.0].
+        bp_bounds (np.ndarray): The bounds for the binding potential parameter. Defaults to [0.5, 0.0, 10.0].
+
+    Returns:
+        tuple: (``fit_parameters``, ``fit_covariance``). Output from `scipy.optimize.curve_fit`.
+        
+    Raises:
+        AssertionError: If the target TAC and times are different dimensions.
+
+    
+    Note:
+        Bounds for :math:`R_{1}`, :math:`k_{2}` and binding potential are applied during optimization to limit the
+        search space of the optimization routine to physically relevant parameters.
+
+    """
     def _fitting_srtm(tac_times, r1, k2, bp):
         return calc_srtm_tac(tac_times=tac_times, r1=r1, k2=k2, bp=bp, ref_tac_vals=ref_tac_vals)
     
