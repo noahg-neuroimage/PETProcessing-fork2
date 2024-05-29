ffa39ef391ed6e0174f342c191c7be1214e88b26
Furqan Dar <darf@kenyon.edu>
2024-05-29 14:14:03 -0500

    Added a safe TAC loading function (which should be replaced in the future).
    This update introduces a new function, `_safe_load_tac`, which is responsible for loading time-activity curves (TAC) from files. This function takes a file name as input, attempts to load a TAC from the specified file, and raises an exception if it encounters an error. The returned result is a numpy array with two indexes representing times and activity respectively.

--- pet_cli/tac_fitting.py
+++ pet_cli/tac_fitting.py
@@ -620,6 +620,31 @@ class TACFitterWithoutBloodVolume(TACFitter):
         return self.tcm_func(x, self.p_tac_vals, *params, vb=0.0)[1]
 
 
+# TODO: Use the safe loading of TACs function from an IO module when it is implemented
+def _safe_load_tac(filename: str, **kwargs) -> np.ndarray:
+    """
+    Loads time-activity curves (TAC) from a file.
+
+    Tries to read a TAC from specified file and raises an exception if unable to do so. We assume that the file has two
+    columns, the first corresponding to time and second corresponding to activity.
+
+    Args:
+        filename (str): The name of the file to be loaded.
+
+    Returns:
+        np.ndarray: A numpy array containing the loaded TAC. The first index corresponds to the times, and the second
+        corresponds to the activity.
+
+    Raises:
+        Exception: An error occurred loading the TAC.
+    """
+    try:
+        return np.array(np.loadtxt(filename).T, dtype=float, order='C', **kwargs)
+    except Exception as e:
+        print(f"Couldn't read file {filename}. Error: {e}")
+        raise e
+
+
 class FitTCMToTAC(object):
     def __init__(self,
                  input_tac_path: str,
