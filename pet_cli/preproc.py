90b9c93a3cd785b8efa395e8700d7bc13f5b3814
noahg-neuroimage <noahg@wustl.edu>
2024-05-30 15:29:13 -0500

    Adding docs to functions without them.

--- pet_cli/preproc.py
+++ pet_cli/preproc.py
@@ -4,7 +4,7 @@ neuroimaging data for a PET study. Acts as a wrapper for other tools supplied
 in ``PPM``.
 
 TODO:
-    * Check if input files exist, through error if no
+    * Check if input files exist, throw error if no
 
 """
 import os
@@ -89,7 +89,7 @@ class PreProc():
     .. code-block:: python
         output_directory = '/path/to/processing'
         output_filename_prefix = 'sub-01'
-        sub_01 = pet_cli.image_operations_4d.ImageOps4d(output_directory,output_filename_prefix)
+        sub_01 = pet_cli.preproc.PreProc(output_directory,output_filename_prefix)
         params = {
             'FilePathWSSInput': '/path/to/pet.nii.gz',
             'FilePathAnat': '/path/to/mri.nii.gz',
@@ -137,10 +137,20 @@ class PreProc():
             * FilePathTACInput (str): Path to PET file with which TACs are computed.
             * FilePathSeg (str): Path to a segmentation image in anatomical space.
             * FilePathLabelMap (str): Path to a label map file, indexing segmentation values to ROIs.
+            * FilePathAtlas (str): Path to atlas image, e.g. MNI152 T1 atlas.
+            * FilePathSUVRInput (str): Path to summed or parametric image on which to normalize to SUVR.
+            * FilePathBlurInput (str): Path to image to blur with a gaussian kernal.
+            * FilePathFSLPremat (str): Path to initial affine transform matrix in FSL format, used for FSL type warping.
+            * FilePathFSLPostmat (str): Path to post-warp affine transform matrix in FSL format, used for FSL type warping.
+            * FilePathWarpRef (str): Path to reference used to compute warp to atlas space. Typically anatomical scan.
+            * FilePathAntsXfm (str): Path to list of Ants transforms used to apply ANTs type warping.
+            * HalfLife (float): Half life of radioisotope. Used for a number of tools.
             * MotionTarget (str | tuple): Target for transformation methods. See :meth:`determine_motion_target`.
             * MocoPars (keyword arguments): Keyword arguments fed into the method call :meth:`ants.motion_correction`.
-            * RegPars (keyword arguments): Keyword arguments fed into the method call :meth:`ants.registration`.
-            * HalfLife (float): Half life of radioisotope.
+            * RegPars (keyword arguments): Keyword arguments fed into the method call :meth:`ants.registration` while performing PET to anat registration.
+            * WarpPars (keyword arguments): Keyword arguments fed into the method call :meth:`ants.registration` while performing PET to atlas registration. 
+            * RefRegion (int): Reference region used to normalize SUVR.
+            * BlurSize (float): Size of gaussian kernal used to blur image.
             * RegionExtract (int): Region index in the segmentation image to extract TAC from, if running TAC on a single ROI.
             * TimeFrameKeyword (str): Keyword in metadata file corresponding to frame timing array to be used in analysis.
             * Verbose (bool): Set to ``True`` to output processing information.
@@ -232,6 +242,16 @@ class PreProc():
     def _generate_outfile_path(self,
                                method_short: str,
                                extension: str='nii.gz'):
+        """
+        Generate the path to an output file, from the output directory,
+        filename prefix, abbreviation of the method name, and filename
+        extension.
+
+        Args:
+            method_short (str): Abbreviation of the method to generate outfile.
+            extension (str): File type extension to return. Defaults to
+            'nii.gz'.
+        """
         output_file_name = f'{self.output_filename_prefix}_{method_short}.{extension}'
         return os.path.join(self.output_directory,output_file_name)
 
