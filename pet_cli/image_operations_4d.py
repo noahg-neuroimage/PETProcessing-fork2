c1ad302f957bd3fe73d1080a4868204bf492d433
Furqan Dar <darf@kenyon.edu>
2024-05-30 14:35:52 -0500

    Tiny fix for docs.

--- pet_cli/image_operations_4d.py
+++ pet_cli/image_operations_4d.py
@@ -72,11 +72,10 @@ def weighted_series_sum(input_image_4d_path: str,
             calculation begins. Must be used with ``end_time``. Default value 0.
         end_time (float): Time, relative to scan start in seconds, at which
             calculation ends. Use value ``-1`` to use all frames in image series.
-            If equal to ``start_time`, one frame at start_time is used. Default value -1.
+            If equal to ``start_time``, one frame at start_time is used. Default value -1.
 
     Returns:
-        summed_image (np.ndarray): 3D image array, in the same space as the input,
-            with the weighted sum calculation applied.
+        np.ndarray: 3D image array, in the same space as the input, with the weighted sum calculation applied.
 
     Raises:
         ValueError: If ``half_life`` is zero or negative.
