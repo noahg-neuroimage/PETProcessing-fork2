636623c1ffdc5f5ec7410f8d44048efd8198e395
Furqan Dar <darf@kenyon.edu>
2024-05-30 10:45:22 -0500

    Enhanced functionality of TAC fitting tool
    - Added arguments for linear interpolation of TACs and to ignore blood volume in fitting
    - Introduced a new function to handle parameter bound generation
    - Implemented appropriate handling of the new arguments in main function
    - Upgraded analysis output with more detailed print statements when print flag is set.

--- pet_cli/cli_tac_fitting.py
+++ pet_cli/cli_tac_fitting.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import argparse
 from . import tac_fitting as pet_fit
 
@@ -30,14 +31,45 @@ def _generate_args() -> argparse.Namespace:
                               help="Upper bounds for each fitting parameter.")
     grp_analysis.add_argument("-f", "--max-fit-iterations", required=False, default=2500, type=int,
                               help="Maximum number of function iterations")
-    
+    grp_analysis.add_argument("-n", "--resample-num", required=False, default=512, type=int,
+                              help="Number of samples for linear interpolation of provided TACs.")
+    grp_analysis.add_argument("-b", "--ignore-blood-volume", required=False, default=False, action='store_true',
+                              help="Whether to ignore any blood volume contributions while fitting")
     
     # Printing arguments
     grp_verbose = parser.add_argument_group('Additional Options')
     grp_verbose.add_argument("--print", action="store_true", help="Whether to print the analysis results.")
     
-    
     return parser.parse_args()
+
+
+def _generate_bounds(initial: list, lower: list, upper: list) -> np.ndarray:
+    return np.asarray(np.asarray([initial, lower, upper]).T)
 
 def main():
     args = _generate_args()
+    
+    bounds = _generate_bounds(initial=args.initial_guesses, lower=args.lower_bounds, upper=args.upper_bounds)
+    
+    tac_fitting = pet_fit.FitTCMToTAC(input_tac_path=args.input_tac_path,
+                                      roi_tac_path=args.roi_tac_path,
+                                      output_directory=args.output_directory,
+                                      output_filename_prefix=args.output_filename_prefix,
+                                      compartment_model=args.model,
+                                      parameter_bounds=bounds,
+                                      weights=None,
+                                      resample_num=args.resample_num,
+                                      aif_fit_thresh_in_mins=args.input_fitting_threshold_in_mins,
+                                      max_func_iters=args.max_fit_iterations,
+                                      ignore_blood_volume=args.ignore_blood_volume
+                                      )
+    tac_fitting.run_analysis()
+    tac_fitting.save_analysis()
+    
+    if args.print:
+        print(tac_fitting.analysis_props["FitResults"]["FitValues"])
+        print(tac_fitting.analysis_props["FitResults"]["FitStdErr"])
+    
+if __name__ == "__main__":
+    main()
+    
