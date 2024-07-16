baab5e08aa9977c33aeec1dc6fb04d6a9db7b121
Furqan Dar <darf@kenyon.edu>
2024-07-16 16:02:33 -0500

    Added the bounded version of the SRTM2 fitting function. Will move on to FRTM2 (bounded and unbounded)

--- petpal/kinetic_modeling/reference_tissue_models.py
+++ petpal/kinetic_modeling/reference_tissue_models.py
@@ -273,7 +273,7 @@ def fit_srtm2_to_tac(tgt_tac_vals: np.ndarray,
                      r1_start: float = 0.5,
                      bp_start: float = 0.5) -> tuple:
     r"""
-    Fit SRTM to the provided target Time Activity Curve (TAC), given the reference TAC, times, and starting guesses for
+    Fit SRTM2 to the provided target Time Activity Curve (TAC), given the reference TAC, times, and starting guesses for
     the kinetic parameters.
 
     .. important::
@@ -284,10 +284,10 @@ def fit_srtm2_to_tac(tgt_tac_vals: np.ndarray,
     parameters.
 
     Args:
-        tgt_tac_vals (np.ndarray): Target TAC to fit with the SRTM.
+        tgt_tac_vals (np.ndarray): Target TAC to fit with the SRTM2.
         ref_tac_times (np.ndarray): Reference TAC values.
         ref_tac_vals (np.ndarray): Reference (and Target) TAC times.
-        k2_prime (float): The :math:`k_2^\prime` value.`
+        k2_prime (float): The :math:`k_2^\prime` value.` Defaults to 0.5.
         r1_start (float): Starting guess for the :math:`R_1\equiv\frac{k_1^\prime}{k_1}` parameter.
         bp_start (float): Starting guess for the binding potential.
 
@@ -357,6 +357,55 @@ def fit_srtm_to_tac_with_bounds(tgt_tac_vals: np.ndarray,
     
     return sp_fit(f=_fitting_srtm, xdata=ref_tac_times, ydata=tgt_tac_vals,
                   p0=st_values, bounds=[lo_values, hi_values])
+
+
+def fit_srtm2_to_tac_with_bounds(tgt_tac_vals: np.ndarray,
+                                 ref_tac_times: np.ndarray,
+                                 ref_tac_vals: np.ndarray,
+                                 k2_prime: int = 0.5,
+                                 r1_bounds: np.ndarray = np.asarray([0.5, 0.0, 10.0]),
+                                 bp_bounds: np.ndarray = np.asarray([0.5, 0.0, 10.0])) -> tuple:
+    r"""
+    Fit SRTM2 to the provided target Time Activity Curve (TAC), given the reference TAC, times, and bounds for
+    the kinetic parameters.
+
+    .. important::
+        This function assumes that the reference TAC is uniformly sampled with respect to time since we perform
+        convolutions.
+
+    This function is a wrapper around `scipy.optimize.curve_fit` and uses parameter bounds during optimization. The
+    bounds for each parameter are formatted as: ``(starting_value, lo_bound, hi_bound)``.
+
+    Args:
+        tgt_tac_vals (np.ndarray): Target TAC to fit with the SRTM2.
+        ref_tac_times (np.ndarray): Times of the reference TAC data.
+        ref_tac_vals (np.ndarray): Reference TAC values.
+        k2_prime (int): The value for :math:`k_2^\prime`. Defaults to 0.5.
+        r1_bounds (np.ndarray): The bounds for the :math:`R_1\equiv\frac{k_1^\prime}{k_1}` parameter.
+        Defaults to [0.5, 0.0, 10.0].
+        bp_bounds (np.ndarray): The bounds for the binding potential parameter. Defaults to [0.5, 0.0, 10.0].
+
+    Returns:
+        tuple: (``fit_parameters``, ``fit_covariance``). Output from `scipy.optimize.curve_fit`.
+
+    Raises:
+        AssertionError: If the target TAC and times are different dimensions.
+
+    See Also:
+        * :func:`calc_srtm_tac`
+        * :func:`fit_srtm2_tac`
+        * :func:`fit_srtm_to_tac_with_bounds`
+
+    """
+    
+    def _fitting_srtm(tac_times, r1, bp):
+        return calc_srtm_tac(tac_times=tac_times, ref_tac_vals=ref_tac_vals, r1=r1, k2=k2_prime, bp=bp)
+    
+    st_values = (r1_bounds[0], bp_bounds[0])
+    lo_values = (r1_bounds[1], bp_bounds[1])
+    hi_values = (r1_bounds[2], bp_bounds[2])
+    
+    return sp_fit(f=_fitting_srtm, xdata=ref_tac_times, ydata=tgt_tac_vals, p0=st_values, bounds=[lo_values, hi_values])
 
 
 def fit_frtm_to_tac(tgt_tac_vals: np.ndarray,
