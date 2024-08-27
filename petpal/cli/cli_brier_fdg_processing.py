f3dc0a20e730e10c6c359722f212b01d13b697c1
Furqan Dar <darf@kenyon.edu>
2024-08-27 11:31:00 -0500

    Starting the function that will do most of the heavy lifting.

---
+++ petpal/cli/cli_brier_fdg_processing.py
@@ -0,0 +1,12 @@
+from ..preproc import preproc
+
+
+
+
+
+def fdg_protocol(sub_ses_string: str,
+                 pet_dir: str,
+                 anat_dir: str,
+                 out_dir: str):
+    
+    pass
