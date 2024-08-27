2961797b2303fc2a2c29ef0dedfb49c336efe759
Furqan Dar <darf@kenyon.edu>
2024-08-27 13:25:55 -0500

    Can remove the hacky fix since file-path generation can handle modality.

--- petpal/preproc/preproc.py
+++ petpal/preproc/preproc.py
@@ -383,7 +383,7 @@ class PreProc():
             )
         
         elif method_name=='thresh_crop':
-            outfile = self._generate_outfile_path(method_short='desc_threshcropped_pet')
+            outfile = self._generate_outfile_path(method_short='threshcropped')
             thresh_crop(input_image_path=preproc_props['FilePathCropInput'],
                         out_image_path=outfile,
                         thresh_val=preproc_props['CropThreshold'],
