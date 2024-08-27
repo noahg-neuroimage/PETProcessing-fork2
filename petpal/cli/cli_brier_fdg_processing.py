c8cf5f314df31e897d1b0de5b22c04fa92e43351
Furqan Dar <darf@kenyon.edu>
2024-08-27 11:57:17 -0500

    Refactor `fdg_protocol` function signature and initialize preprocessing properties.
    - Updated `fdg_protocol` to accept separate subject and session IDs instead of a single combined string.
    - Added initialization of `sub_ses_prefix` and absolute path for `bids_root_dir`.
    - Introduced `preproc_props` dictionary to store preprocessing properties.
    - Initialized `PreProc` object with the specified output directory and filename prefix.

--- petpal/cli/cli_brier_fdg_processing.py
+++ petpal/cli/cli_brier_fdg_processing.py
@@ -1,12 +1,31 @@
 from ..preproc import preproc
+import os
 
 
 
 
-
-def fdg_protocol(sub_ses_string: str,
+def fdg_protocol(bids_root_dir: str,
+                 sub_id: str,
+                 ses_id: str,
                  pet_dir: str,
                  anat_dir: str,
                  out_dir: str):
     
-    pass
+    sub_ses_prefix = f'sub-{sub_id}_ses-{ses_id}'
+    
+    BIDS_ROOT = os.path.abspath(bids_root_dir)
+    
+    
+    preproc_props = {
+        'FilePathLabelMap': '/data/brier/CMMS_BIDS/derivatives/ROI_mask/dseg_forCMMS.tsv',
+        'HalfLife': 6586.2,
+        'StartTimeWSS':0,
+        'EndTimeWSS':600,
+        'MotionTarget': (0, 600),
+        'RegPars': {'aff_metric': 'mattes', 'type_of_transform': 'DenseRigid'},
+        'TimeFrameKeyWord': 'FrameTimeStart',
+        'Verbose': True,
+        }
+    
+    sub_preproc = preproc.PreProc(output_directory=out_dir,
+                                  output_filename_prefix=f'sub-{sub_id}_ses-{ses_id}')
