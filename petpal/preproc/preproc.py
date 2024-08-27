3f416ae07b656c937730300277b6d502230b2e65
Furqan Dar <darf@kenyon.edu>
2024-08-27 13:08:58 -0500

    - Hacky way to get the file-name of the cropped output to be more BIDS consistent. Probably a better idea to modify the `_generate_outfile_path` method in preproc.py to include modality and descriptors.

--- petpal/preproc/preproc.py
+++ petpal/preproc/preproc.py
@@ -377,7 +377,7 @@ class PreProc():
             )
         
         elif method_name=='thresh_crop':
-            outfile = self._generate_outfile_path(method_short='threshcrop')
+            outfile = self._generate_outfile_path(method_short='desc_threshcropped_pet')
             thresh_crop(input_image_path=preproc_props['FilePathCropInput'],
                         out_image_path=outfile,
                         thresh_val=preproc_props['CropThreshold'],
