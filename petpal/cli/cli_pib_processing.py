0344fe615b543459dd30845ee72b84911fc48749
kenanoestreich <kenan.oestreich@gmail.com>
2024-09-12 11:23:09 -0500

    feat: add skip flags and parallelization

--- petpal/cli/cli_pib_processing.py
+++ petpal/cli/cli_pib_processing.py
@@ -21,7 +21,7 @@ Assumptions regarding input data:
 
 Example:
     .. code-block:: bash
-        TODO: update this once changes have been made
+        petpal-pib-proc -i bids_dir/ -l labelmap.tsv -v
 
 See Also:
     :mod:`petpal.preproc` - module for initiating and saving the graphical analysis of PET parametric images.
@@ -36,11 +36,12 @@ import re
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 import petpal.preproc
+from multiprocessing import Pool
 
 logger = logging.getLogger(__name__)
 
 
-def parse_command_line_args() -> argparse.Namespace:
+def _parse_command_line_args() -> argparse.Namespace:
     """
     Retrieves arguments and flags from stdin using argparse.ArgumentParser().
 
@@ -50,12 +51,16 @@ def parse_command_line_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Script to process PiB PET data from 4D-PET to SUVR Parametric Image',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-ib', '--input_bids', required=True, help='Path to the top-level BIDS directory')
-    parser.add_argument('-d', '--input_dseg', required=True, help='Path to dseg.tsv file containing LUT info')
-    parser.add_argument('-v', '--verbose', help='Display more information while running', action='store_true')
+    parser.add_argument('-i', '--input_bids', required=True, help='Path to the top-level BIDS directory')
+    parser.add_argument('-l', '--label_map', required=True, help='Path to .tsv file containing segmentation LUT info')
+    parser.add_argument('-v', '--verbose', help='Display more information while running (up to -vv for debug mode)',
+                        action='count', default=0)
     parser.add_argument('-n', '--dry_run', help='Do not perform any computations - Useful for sanity checks; '
                                                 'equivalent to using all --skip-{command} flags',
                         action='store_true')
+    parser.add_argument('-t', '--threads',
+                        help='Number of threads to use for parallel processing; no parallel processing occurs if -t/--threads not specified',
+                        default=1)
     parser.add_argument('--skip-motion-correct', help='Do not perform motion correction', action='store_true')
     parser.add_argument('--skip-register', help='Do not perform registration', action='store_true')
     parser.add_argument('--skip-tacs', help='Do not generate TACs', action='store_true')
@@ -69,7 +74,7 @@ def process_single_subject_session(preproc_instance: petpal.preproc.preproc.PreP
                                    skip_motion_correct: bool = False,
                                    skip_register: bool = False,
                                    skip_tacs: bool = False,
-                                   skip_suvr: bool = False) -> None:
+                                   skip_suvr: bool = False,) -> None:
     """
     Processes one subject-session pair using an instance of the :class:`petpal.preproc.preproc.PreProc` class.
 
@@ -81,12 +86,16 @@ def process_single_subject_session(preproc_instance: petpal.preproc.preproc.PreP
 
     """
 
-    progress_bar = tqdm(total=4)
+    # Adjust progress bar for skipped steps
+    num_steps = (skip_motion_correct, skip_register, skip_tacs, skip_suvr).count(False)
+    if num_steps > 0:
+        progress_bar = tqdm(total=num_steps)
+
     with logging_redirect_tqdm():
 
         # Step 1: Run Motion Correction on 4DPET image
-        logger.debug(f'Starting Motion Correction Process for {preproc_instance.preproc_props["FilePathMocoInp"]}')
         if not skip_motion_correct:
+            logger.debug(f'Performing Motion Correction on {preproc_instance.preproc_props["FilePathMocoInp"]}')
             try:
                 preproc_instance.run_preproc(method_name='motion_corr')
             except KeyError as e:
@@ -95,21 +104,22 @@ def process_single_subject_session(preproc_instance: petpal.preproc.preproc.PreP
                     f'\n{e}\n'
                     f'Skipping this subject-session and continuing...\n')
                 return
+            progress_bar.update(1)
 
         # Step 2: Register MoCo'd PET to MRI space
-        progress_bar.update(1)
-        logger.debug(
-            f'Starting Registration Process for motion-corrected PET at '
-            f'{preproc_instance.preproc_props["FilePathRegInp"]} to {preproc_instance.preproc_props["FilePathAnat"]}')
         if not skip_register:
+            logger.debug(
+                f'Registering {preproc_instance.preproc_props["FilePathRegInp"]} to '
+                f'{preproc_instance.preproc_props["FilePathAnat"]} space')
             preproc_instance.run_preproc(method_name='register_pet')
+            progress_bar.update(1)
 
         # Step 3: Extract TACs from each Brain ROI using Freesurfer Segmentation Input
-        progress_bar.update(1)
-        logger.debug(
-            f'Starting TAC Extraction Process for Registered PET using segmentation '
-            f'{preproc_instance.preproc_props["FilePathSeg"]}')
+
         if not skip_tacs:
+            logger.debug(
+                f'Extracting TACs from registered PET using segmentation '
+                f'{preproc_instance.preproc_props["FilePathSeg"]}')
             try:
                 preproc_instance.run_preproc(method_name='write_tacs')
             except ValueError:
@@ -118,17 +128,17 @@ def process_single_subject_session(preproc_instance: petpal.preproc.preproc.PreP
                     f'{preproc_instance.preproc_props["FilePathSeg"]} to anatomical space')
                 preproc_instance.run_preproc(method_name='resample_segmentation')
                 preproc_instance.run_preproc(method_name='write_tacs')
-
+            progress_bar.update(1)
 
         # Step 4: Compute SUVR with Cerebellar Gray Matter as Reference Region
-        progress_bar.update(1)
-        logger.debug(
-            f'Starting SUVR Process for Registered and Motion-Corrected copy of '
-            f'{preproc_instance.preproc_props["FilePathRegInp"]}')
+
         if not skip_suvr:
+            logger.debug(
+                f'Starting SUVR Process for Registered and Motion-Corrected copy of '
+                f'{preproc_instance.preproc_props["FilePathRegInp"]}')
             preproc_instance.run_preproc(method_name='weighted_series_sum')
             preproc_instance.run_preproc(method_name='suvr')
-
+            progress_bar.update(1)
 
     return
 
@@ -137,28 +147,35 @@ def main():
     """
     PiB Pipeline Command Line Interface.
     """
-    # Unpack command line arguments
-    args = parse_command_line_args()
 
-    logger.setLevel(level=logging.INFO)
+    # Unpack command line arguments
+    args = _parse_command_line_args()
+
+    bids_dir = os.path.abspath(args.input_bids)
+    label_map_path = os.path.abspath(args.label_map)
+    dry_run = args.dry_run
     verbose = args.verbose
-    if verbose:
-        logger.setLevel(level=logging.DEBUG)
+    skip_motion_correct = args.skip_motion_correct
+    skip_register = args.skip_register
+    skip_tacs = args.skip_tacs
+    skip_suvr = args.skip_suvr
+    threads = args.threads
+    if dry_run:
+        skip_motion_correct, skip_register, skip_tacs, skip_suvr = True, True, True, True
+
+    # Python logging levels are ints (0, 10, 20, 30, 40, 50) with 10 being debug, 20 being info, etc...
+    logger.setLevel(level=logging.WARNING - (10 * verbose)) if (verbose < 3) else logger.setLevel(level=logging.DEBUG)
 
     if not (os.path.exists(args.input_bids)):
         raise FileNotFoundError(f'No such directory {args.input_bids}')
 
-    if os.path.exists(args.input_dseg):
-        dseg_path = args.input_dseg
-    else:
-        raise FileNotFoundError(f'No such file {args.input_dseg}')
+    if not os.path.exists(args.label_map):
+        raise FileNotFoundError(f'No such file {args.label_map}')
 
-    force = args.force
-    dry_run = args.dry_run
     half_life = 1224  # C-11 Half-Life TODO: Make a shared global dict with Half-Lives of common Radiotracers
 
     # Parse through BIDS directory to define input and output paths
-    bids_dir = os.path.abspath(args.input_bids)
+
     subjects_pattern = re.compile(r'sub-(\d+)')
     sessions_pattern = re.compile(r'ses-(\d+)')
 
@@ -177,10 +194,14 @@ def main():
                 session_id = sessions_pattern.match(session_dir.name).group(1)  # Retrieve session id
                 prefix = f'sub-{subject_id}_ses-{session_id}'
                 sub_ses_props = {
-                    'FilePathMocoInp': glob.glob(f'{session_dir.path}/pet/sub-{subject_id}_ses-{session_id}_pet.nii.gz')[0],
-                    'FilePathAnat': glob.glob(f'{session_dir.path}/anat/sub-{subject_id}_ses-{session_id}_T1w.nii.gz')[0],
+                    'FilePathMocoInp':
+                        glob.glob(f'{session_dir.path}/pet/sub-{subject_id}_ses-{session_id}_pet.nii.gz')[0],
+                    'FilePathAnat': glob.glob(f'{session_dir.path}/anat/sub-{subject_id}_ses-{session_id}_T1w.nii.gz')[
+                        0],
                     'FilePathSeg':
-                        glob.glob(f'{bids_dir}/derivatives/freesurfer/sub-{subject_id}/ses-{session_id}/sub-{subject_id}_ses-{session_id}_aparc+aseg.nii.gz')[0]
+                        glob.glob(
+                            f'{bids_dir}/derivatives/freesurfer/sub-{subject_id}/ses-{session_id}/sub-{subject_id}_ses-{session_id}_aparc+aseg.nii.gz')[
+                            0]
                 }
                 output = f'{bids_dir}/derivatives/petpal/sub-{subject_id}/ses-{session_id}'
                 subject = petpal.preproc.preproc.PreProc(output_directory=output,
@@ -194,7 +215,8 @@ def main():
                 'FilePathMocoInp': glob.glob(f'{subject_dir.path}/pet/sub-{subject_id}_pet.nii.gz')[0],
                 'FilePathAnat': glob.glob(f'{subject_dir.path}/anat/sub-{subject_id}_T1w.nii.gz')[0],
                 'FilePathSeg':
-                    glob.glob(f'{bids_dir}/derivatives/freesurfer/sub-{subject_id}/sub-{subject_id}_aparc+aseg.nii.gz')[0],
+                    glob.glob(f'{bids_dir}/derivatives/freesurfer/sub-{subject_id}/sub-{subject_id}_aparc+aseg.nii.gz')[
+                        0],
             }
             output = os.path.join(bids_dir, 'derivatives/petpal', f'sub-{subject_id}')
             subject = petpal.preproc.preproc.PreProc(output_directory=output,
@@ -202,24 +224,28 @@ def main():
             subject.update_props(sub_props)
             subject_inputs.append(subject)
 
-    logger.info(f'Found {len(subject_inputs)} subject-session pairs.')
+    logger.debug(f'Found {len(subject_inputs)} subject-session pairs.')
 
     for subject_info in subject_inputs:
         # Update props with everything necessary that's not found in BIDS input directory.
         properties = {
             'FilePathWSSInput': subject_info._generate_outfile_path(method_short='reg'),
             'HalfLife': half_life,
-            'Verbose': verbose,
+            'Verbose': True if (verbose > 0) else False,
             'MotionTarget': (0, 600),  # Use the summed first 10 minutes as motion target
             'FilePathRegInp': subject_info._generate_outfile_path(method_short='moco'),
             'FilePathTACInput': subject_info._generate_outfile_path(method_short='reg'),
-            'FilePathLabelMap': dseg_path,
+            'FilePathLabelMap': label_map_path,
             'TimeFrameKeyword': 'FrameTimesStart',
             'FilePathSUVRInput': subject_info._generate_outfile_path(method_short='wss'),
             'RefRegion': 8,
         }
         subject_info.update_props(properties)
-        process_single_subject_session(subject_info, dry_run=dry_run, force=force)
+
+    with Pool(threads) as p:
+        p.starmap(process_single_subject_session,
+                  map(lambda subject_dict: (subject_dict, skip_motion_correct, skip_register, skip_tacs, skip_suvr),
+                      subject_inputs))
 
 
 if __name__ == "__main__":
