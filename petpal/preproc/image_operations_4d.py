29579e9295b574863dfd560d27f8bb6e0bc9e1f5
noahg-neuroimage <noahg@wustl.edu>
2024-09-17 16:32:44 -0500

    Basic implementation of brain masking algorithm

--- petpal/preproc/image_operations_4d.py
+++ petpal/preproc/image_operations_4d.py
@@ -21,7 +21,9 @@ from scipy.interpolate import interp1d
 from scipy.ndimage import center_of_mass
 import nibabel
 import numpy as np
+import ants
 from ..utils import image_io, math_lib
+from ..preproc.motion_corr import determine_motion_target
 
 
 def crop_image(input_image_path: str,
@@ -66,6 +68,46 @@ def crop_image(input_image_path: str,
     image_io.safe_copy_meta(input_image_path=input_image_path,
                             out_image_path=out_image_path)
     return cropped_image
+
+
+def brain_mask(input_image_4d_path: str,
+               out_image_path: str,
+               atlas_image_path: str,
+               atlas_mask_path: str,
+               motion_target_option='mean_image',
+               half_life: float=None):
+    """
+    Create a brain mask for a PET image. Create target PET image, which is then warped to a
+    provided anatomical atlas. The transformation to atlas space is then applied to transform a
+    provided mask in atlas space into PET space. This mask can then by used in various operations.
+
+    Args:
+        input_image_4d_path (str): Path to input 4D PET image.
+        out_image_path (str): Path to which brain mask in PET space is written.
+        atlas_image_path (str): Path to anatomical atlas image.
+        atlas_mask_path (str): Path to brain mask in atlas space.
+        motion_target_option: Used to determine 3D target in PET space. Default 'mean_image'.
+    """
+    atlas = ants.image_read(atlas_image_path)
+    atlas_mask = ants.image_read(atlas_mask_path)
+    pet_ref = ants.image_read(determine_motion_target(
+        motion_target_option=motion_target_option,
+        input_image_4d_path=input_image_4d_path,
+        half_life=half_life
+    ))
+    xfm = ants.registration(
+        fixed=atlas,
+        moving=pet_ref,
+        type_of_transform='SyN'
+    )
+    mask_on_pet = ants.apply_transforms(
+        fixed=pet_ref,
+        moving=atlas_mask,
+        transformlist=xfm['invtransforms'],
+        interpolator='nearestNeighbor'
+    )
+    mask = mask_on_pet.get_mask()
+    ants.image_write(image=mask,filename=out_image_path)
 
 
 def weighted_series_sum(input_image_4d_path: str,
