d83f5837095282c91d342f770f79428ab8615384
Furqan Dar <darf@kenyon.edu>
2024-09-19 14:10:34 -0500

    Refactor pipeline step validation and step listing methods
    - Extract get_empty_default_kwargs method for clean separation of concerns in argument validation.
    - Simplify the __str__ method by removing unnecessary separators.
    - Update step listing with detailed and name-only options for preproc and kinetic_modeling steps.
    - Correct method names with typos from stes to steps for clarity.

--- petpal/pipelines/pipelines.py
+++ petpal/pipelines/pipelines.py
@@ -29,13 +29,21 @@ class GenericStep:
                     unset_args_dict[arg_name] = arg_val.default
         return unset_args_dict
     
-    def validate_kwargs_for_non_default_have_been_set(self):
+    def get_empty_default_kwargs(self):
         unset_args_dict = self.get_non_unset_args_from_function()
+        empty_kwargs = []
         for arg_name, arg_val in unset_args_dict.items():
             if arg_val is inspect.Parameter.empty:
                 if arg_name not in self.kwargs:
-                    raise ValueError(f"{self._func_name}'s argument {arg_name} does not have a default value "
-                                     f"and must be set!")
+                    empty_kwargs.append(arg_name)
+        return empty_kwargs
+        
+    
+    def validate_kwargs_for_non_default_have_been_set(self):
+        empty_kwargs = self.get_empty_default_kwargs()
+        if empty_kwargs:
+            unset_args = '\n'.join(empty_kwargs)
+            raise RuntimeError(f"For {self._func_name}, the following arguments must be set:\n{unset_args}")
         
     
     def execute(self) -> None:
@@ -45,15 +53,13 @@ class GenericStep:
         
         
     def __str__(self):
-        info_str = ['-'*80,
-                    f'({type(self).__name__} Info):',
+        info_str = [f'({type(self).__name__} Info):',
                     f'Step Name: {self.name}',
                     f'Function Name: {self._func_name}',
                     'Arguments Set:',
                     f'{self.kwargs}',
                     'Default Arguments:',
-                    f'{self.get_non_unset_args_from_function()}',
-                    '-'*80]
+                    f'{self.get_non_unset_args_from_function()}']
         return '\n'.join(info_str)
 
 
@@ -80,7 +86,7 @@ class ImageToImageStep(GenericStep):
         unset_args_dict = ArgsDict()
         func_params = self.func_sig.parameters
         for arg_name, arg_val in list(func_params.items())[2:]:
-            if arg_name not in self.kwargs:
+            if (arg_name not in self.kwargs) and ('kwargs' not in arg_name):
                     unset_args_dict[arg_name] = arg_val.default
         return unset_args_dict
     
@@ -102,10 +108,21 @@ class GenericPipeline:
     def add_step(self, step: GenericStep) -> None:
         self.steps.append(step)
         
-    def list_steps(self) -> None:
-        for step in self.steps:
-            print(step)
+    def list_step_details(self) -> None:
+        print(f"(Pipeline Info):")
+        for step_id, a_step in enumerate(self.steps):
+            print('-' * 80)
+            print(f"Step Number {step_id+1}")
+            print(a_step)
+            print('-' * 80)
         
+    def list_step_names(self) -> None:
+        print(f"(Pipeline Info):")
+        print('-' * 80)
+        for step_id, a_step in enumerate(self.steps):
+            print(f"Step Number {step_id+1}: {a_step.name}")
+        print('-' * 80)
+    
     def run_steps(self) -> None:
         for step in self.steps:
             step.execute()
@@ -129,8 +146,14 @@ class ProcessingPipeline(object):
     def add_kinetic_modeling_step(self, step: GenericStep) -> None:
         self.kinetic_modeling.add_step(step)
     
-    def list_preproc_steps(self) -> None:
-        self.preproc.list_steps()
+    def list_preproc_stes(self) -> None:
+        self.preproc.list_step_names()
     
-    def list_kinetic_modeling_steps(self) -> None:
-        self.kinetic_modeling.list_steps()
+    def list_kinetic_modeling_stes(self) -> None:
+        self.kinetic_modeling.list_step_names()
+    
+    def list_preproc_step_details(self) -> None:
+        self.preproc.list_step_details()
+    
+    def list_kinetic_modeling_step_details(self) -> None:
+        self.kinetic_modeling.list_step_details()
