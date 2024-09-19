afa7782a5c7927c5af91ebea77f95ee176971a3f
Furqan Dar <darf@kenyon.edu>
2024-09-19 13:22:37 -0500

    Refactor class naming and argument handling
    - Renamed `AbstractStep` and `AbstractPipeline` classes to `GenericStep` and `GenericPipeline` respectively.
    - Introduced `ArgsDict` class to improve string representation of argument dictionaries.
    - Enhanced `__str__` method in `GenericStep` and `ImageToImageStep` for better output formatting.

--- petpal/pipelines/pipelines.py
+++ petpal/pipelines/pipelines.py
@@ -1,13 +1,18 @@
 from typing import Callable
 import inspect
 
+class ArgsDict(dict):
+    def __str__(self):
+        rep_str = [f'    {arg}: {val}' for arg, val in self.items()]
+        return '\n'.join(rep_str)
 
-class AbstractStep:
+
+class GenericStep:
     def __init__(self, name: str, function: Callable, **kwargs) -> None:
         self.name = name
         self.function = function
         self._func_name = function.__name__
-        self.kwargs = kwargs
+        self.kwargs = ArgsDict(kwargs)
         self.func_sig = inspect.signature(self.function)
         self.validate_kwargs_for_non_default_have_been_set()
         
@@ -17,7 +22,7 @@ class AbstractStep:
         Returns:
 
         """
-        unset_args_dict = dict()
+        unset_args_dict = ArgsDict()
         func_params = self.func_sig.parameters
         for arg_name, arg_val in func_params.items():
             if arg_name not in self.kwargs:
@@ -38,26 +43,27 @@ class AbstractStep:
         self.function(**self.kwargs)
         print(f"(Info): Finished {self.name}")
         
+        
     def __str__(self):
-        info_str = ['-'*50,
-                    '(Step Info):',
+        info_str = ['-'*80,
+                    f'({type(self).__name__} Info):',
+                    f'Step Name: {self.name}',
                     f'Function Name: {self._func_name}',
                     'Arguments Set:',
                     f'{self.kwargs}',
                     'Default Arguments:',
                     f'{self.get_non_unset_args_from_function()}',
-                    '-'*50]
+                    '-'*80]
         return '\n'.join(info_str)
 
 
-class ImageToImageStep(AbstractStep):
+class ImageToImageStep(GenericStep):
     def __init__(self, name: str, function: Callable,
                  input_image_path: str, output_image_path: str,
                  **kwargs) -> None:
         super().__init__(name, function, **kwargs)
         self.input_image = input_image_path
         self.output_image = output_image_path
-        
         
     def execute(self) -> None:
         print(f"(Info): Executing {self.name}")
@@ -66,30 +72,39 @@ class ImageToImageStep(AbstractStep):
         
     def get_non_unset_args_from_function(self):
         """
-        Since this class expects to have functions with the signature f(some_input_path, some_output_path, *args),
+        Since this class expects to have functions with the signature f(some_input_path, some_output_path, **kwargs),
         we want to skip the first two arguments
         Returns:
 
         """
-        unset_args_dict = dict()
+        unset_args_dict = ArgsDict()
         func_params = self.func_sig.parameters
         for arg_name, arg_val in list(func_params.items())[2:]:
             if arg_name not in self.kwargs:
                     unset_args_dict[arg_name] = arg_val.default
         return unset_args_dict
+    
+    def __str__(self):
+        io_dict = ArgsDict({'input_image_path': self.input_image,
+                            'output_image_path': self.output_image})
+        sup_str_list = super().__str__().split('\n')
+        args_ind = sup_str_list.index("Arguments Set:")+1
+        sup_str_list.insert(args_ind, f"{io_dict}")
+        
+        return "\n".join(sup_str_list)
         
         
-class AbstractPipeline:
+class GenericPipeline:
     def __init__(self, name: str) -> None:
         self.name = name
         self.steps = []
         
-    def add_step(self, step: AbstractStep) -> None:
+    def add_step(self, step: GenericStep) -> None:
         self.steps.append(step)
         
     def list_steps(self) -> None:
         for step in self.steps:
-            print(step.name)
+            print(step)
         
     def run_steps(self) -> None:
         for step in self.steps:
@@ -99,8 +114,8 @@ class AbstractPipeline:
 class ProcessingPipeline(object):
     def __init__(self, name: str) -> None:
         self.name = name
-        self.preproc = AbstractPipeline('preproc')
-        self.kinetic_modeling = AbstractPipeline('kinetic_modeling')
+        self.preproc = GenericPipeline('preproc')
+        self.kinetic_modeling = GenericPipeline('kinetic_modeling')
     
     def run_preproc(self):
         self.preproc.run_steps()
@@ -108,10 +123,10 @@ class ProcessingPipeline(object):
     def run_kinetic_modeling(self):
         self.kinetic_modeling.run_steps()
     
-    def add_preproc_step(self, step: AbstractStep) -> None:
+    def add_preproc_step(self, step: GenericStep) -> None:
         self.preproc.add_step(step)
     
-    def add_kinetic_modeling_step(self, step: AbstractStep) -> None:
+    def add_kinetic_modeling_step(self, step: GenericStep) -> None:
         self.kinetic_modeling.add_step(step)
     
     def list_preproc_steps(self) -> None:
