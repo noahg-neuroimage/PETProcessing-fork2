2d232248da4e19fd4de082b6e675f270fc8f4829
Furqan Dar <darf@kenyon.edu>
2024-10-04 15:27:30 -0500

    Add BaseFunctionBasedStep class to pipelines_v2.py
    - Introduced `BaseFunctionBasedStep` class with `__init__`, `validate_kwargs_for_non_default_have_been_set`, `get_empty_default_kwargs`, and `execute` methods.
    - Ensured that `execute` method prints informative messages before and after function execution.

--- petpal/pipelines/pipelines_v2.py
+++ petpal/pipelines/pipelines_v2.py
@@ -12,3 +12,25 @@ from ..kinetic_modeling import reference_tissue_models as pet_rtms
 from ..kinetic_modeling import graphical_analysis as pet_grph
 from .pipelines import ArgsDict
 
+
+class BaseFunctionBasedStep():
+    def __init__(self, name: str, function: Callable, *args, **kwargs) -> None:
+        self.name = name
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.func_sig = inspect.signature(self.function)
+        self.validate_kwargs_for_non_default_have_been_set()
+        
+    def validate_kwargs_for_non_default_have_been_set(self) -> None:
+        pass
+    
+    def get_empty_default_kwargs(self):
+        pass
+    
+    def execute(self):
+        print(f"(Info): Executing {self.name}")
+        self.function(*self.args, **self.kwargs)
+        print(f"(Info): Finished {self.name}")
+        
+    
