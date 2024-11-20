82bd2a4efb88bab420c43d7b6c8abd3d45d78c7f
Furqan Dar <darf@kenyon.edu>
2024-11-20 10:48:02 -0600

    Add docstrings to TACsFromSegmentationStep class methods
    This commit adds detailed docstrings to the methods and attributes of the TACsFromSegmentationStep class to enhance code readability and maintainability. Additionally, it updates the method signatures with type annotations where appropriate. The docstrings provide comprehensive information about each method's purpose, arguments, and return values.

--- petpal/pipelines/preproc_steps.py
+++ petpal/pipelines/preproc_steps.py
@@ -10,6 +10,30 @@ from ..utils.bids_utils import parse_path_to_get_subject_and_session_id, snake_t
 from ..utils.image_io import safe_copy_meta
 
 class TACsFromSegmentationStep(FunctionBasedStep):
+    """
+    A step in a processing pipeline for generating Time Activity Curves (TACs) from segmented images.
+
+    This class is specialized for handling the input and output paths related to TAC generation,
+    extending the FunctionBasedStep with specific properties and methods for TACs.
+
+    Attributes:
+        input_image_path (str): Path to the input image.
+        segmentation_image_path (str): Path to the segmentation image.
+        segmentation_label_map_path (str): Path to the segmentation label map.
+        out_tacs_dir (str): Directory where the output TACs will be saved.
+        out_tacs_prefix (str): Prefix for the output TACs.
+        time_keyword (str): Keyword for the time frame, default is 'FrameReferenceTime'.
+        verbose (bool): Verbosity flag, default is False.
+
+    Methods:
+        - set_input_as_output_from(sending_step): Sets the input image path based on the output from
+            a specified sending step.
+        - infer_outputs_from_inputs(out_dir, der_type, suffix=None, ext=None, **extra_desc): Infers
+            output directory and prefix for TACs based on the input image path.
+        - default_write_tacs_from_segmentation_rois(): Provides a class method to create an instance
+            with default parameters.
+        
+    """
     def __init__(self,
                  input_image_path: str,
                  segmentation_image_path: str,
@@ -18,6 +42,18 @@ class TACsFromSegmentationStep(FunctionBasedStep):
                  out_tacs_prefix: str,
                  time_keyword='FrameReferenceTime',
                  verbose=False) -> None:
+        """
+        Initializes a TACsFromSegmentationStep with specified parameters.
+
+        Args:
+            input_image_path (str): Path to the input image.
+            segmentation_image_path (str): Path to the segmentation image.
+            segmentation_label_map_path (str): Path to the segmentation label map.
+            out_tacs_dir (str): Directory where the output TACs will be saved.
+            out_tacs_prefix (str): Prefix for the output TACs.
+            time_keyword (str): Keyword for the time frame, default is 'FrameReferenceTime'.
+            verbose (bool): Verbosity flag, default is False.
+        """
         super().__init__(name='write_roi_tacs', function=write_tacs, input_image_path=input_image_path,
                          segmentation_image_path=segmentation_image_path, label_map_path=segmentation_label_map_path,
                          out_tac_dir=out_tacs_dir, out_tac_prefix=out_tacs_prefix, time_frame_keyword=time_keyword,
@@ -31,6 +67,12 @@ class TACsFromSegmentationStep(FunctionBasedStep):
         self.verbose = verbose
     
     def __repr__(self):
+        """
+        Provides an unambiguous string representation of the TACsFromSegmentationStep instance.
+
+        Returns:
+            str: A string representation showing how the instance can be recreated.
+        """
         cls_name = type(self).__name__
         info_str = [f'{cls_name}(']
         
@@ -47,46 +89,109 @@ class TACsFromSegmentationStep(FunctionBasedStep):
     
     @property
     def segmentation_image_path(self):
+        """
+        Gets the path to the segmentation image.
+
+        Returns:
+            str: The path to the segmentation image.
+        """
         return self._segmentation_image
     
     @segmentation_image_path.setter
-    def segmentation_image_path(self, segmentation_image_path):
+    def segmentation_image_path(self, segmentation_image_path: str):
+        """
+        Sets the path to the segmentation image and updates the function arguments.
+
+        Args:
+            segmentation_image_path (str): The new path to the segmentation image.
+        """
         self._segmentation_image = segmentation_image_path
         self.kwargs['segmentation_image_path'] = segmentation_image_path
     
     @property
     def segmentation_label_map_path(self):
+        """
+        Gets the path to the segmentation label map.
+
+        Returns:
+            str: The path to the segmentation label map.
+        """
         return self._segmentation_label_map
     
     @segmentation_label_map_path.setter
-    def segmentation_label_map_path(self, segmentation_label_map_path):
+    def segmentation_label_map_path(self, segmentation_label_map_path: str):
+        """
+        Sets the path to the segmentation label map and updates the function arguments.
+
+        Args:
+            segmentation_label_map_path (str): The new path to the segmentation label map.
+        """
         self._segmentation_label_map = segmentation_label_map_path
         self.kwargs['label_map_path'] = segmentation_label_map_path
     
     @property
     def out_tacs_dir(self):
+        """
+        Gets the directory where the output TACs will be saved.
+
+        Returns:
+            str: The output directory path.
+        """
         return self._out_tacs_path
     
     @out_tacs_dir.setter
     def out_tacs_dir(self, out_tacs_path: str):
+        """
+        Sets the directory where the output TACs will be saved and updates the function arguments.
+
+        Args:
+            out_tacs_path (str): The new output directory path.
+        """
         self.kwargs['out_tac_dir'] = out_tacs_path
         self._out_tacs_path = out_tacs_path
     
     @property
     def out_tacs_prefix(self):
+        """
+        Gets the prefix for the output TACs.
+
+        Returns:
+            str: The prefix for the output TACs.
+        """
         return self._out_tacs_prefix
     
     @out_tacs_prefix.setter
     def out_tacs_prefix(self, out_tacs_prefix: str):
+        """
+        Sets the prefix for the output TACs and updates the function arguments.
+
+        Args:
+            out_tacs_prefix (str): The new prefix for the output TACs.
+        """
         self.kwargs['out_tac_prefix'] = out_tacs_prefix
         self._out_tacs_prefix = out_tacs_prefix
     
     @property
     def out_path_and_prefix(self):
+        """
+        Gets the output directory path and prefix as a tuple.
+
+        Returns:
+            tuple: A tuple containing the output directory path and prefix.
+        """
         return self._out_tacs_path, self._out_tacs_prefix
     
     @out_path_and_prefix.setter
-    def out_path_and_prefix(self, out_dir_and_prefix):
+    def out_path_and_prefix(self, out_dir_and_prefix: str):
+        """
+        Sets both the output directory path and prefix.
+
+        Args:
+            out_dir_and_prefix (tuple): A tuple containing the output directory and prefix.
+
+        Raises:
+            ValueError: If the provided value is not a tuple with two items.
+        """
         try:
             out_dir, out_prefix = out_dir_and_prefix
         except ValueError:
@@ -97,31 +202,71 @@ class TACsFromSegmentationStep(FunctionBasedStep):
     
     @property
     def input_image_path(self):
+        """
+        Gets the path to the input image.
+
+        Returns:
+            str: The path to the input image.
+        """
         return self._input_image
     
     @input_image_path.setter
     def input_image_path(self, input_image_path: str):
+        """
+        Sets the path to the input image and updates the function arguments.
+
+        Args:
+            input_image_path (str): The new path to the input image.
+        """
         self.kwargs['input_image_path'] = input_image_path
         self._input_image = input_image_path
     
     def set_input_as_output_from(self, sending_step):
+        """
+        Sets the input image path based on the output from a specified sending step.
+
+        Args:
+            sending_step: The step from which to derive the input image path.
+        """
         if isinstance(sending_step, ImageToImageStep):
             self.input_image_path = sending_step.output_image_path
         else:
             super().set_input_as_output_from(sending_step)
     
-    @classmethod
-    def default_write_tacs_from_segmentation_rois(cls):
-        return cls(input_image_path='', segmentation_image_path='', segmentation_label_map_path='', out_tacs_dir='',
-                   out_tacs_prefix='', time_keyword='FrameReferenceTime', verbose=False)
-    
-    def infer_outputs_from_inputs(self, out_dir: str, der_type, suffix=None, ext=None, **extra_desc):
+    def infer_outputs_from_inputs(self, out_dir: str, der_type: str, suffix: str=None, ext: str=None, **extra_desc):
+        """
+        Infers output directory and prefix for TACs based on the input image path.
+
+        Args:
+            out_dir (str): Directory where the outputs will be saved.
+            der_type (str): Type of derivatives.
+            suffix (str, optional): Suffix for the output files. Defaults to None.
+            ext (str, optional): Extension for the output files. Defaults to None.
+            **extra_desc: Additional descriptive parameters.
+        """
         sub_id, ses_id = parse_path_to_get_subject_and_session_id(self.input_image_path)
         outpath = gen_bids_like_dir_path(sub_id=sub_id, ses_id=ses_id, sup_dir=out_dir, modality='tacs')
         self.out_tacs_dir = outpath
         step_name_in_camel_case = snake_to_camel_case(self.name)
         self.out_tacs_prefix = f'sub-{sub_id}_ses-{ses_id}_desc-{step_name_in_camel_case}'
+    
+    @classmethod
+    def default_write_tacs_from_segmentation_rois(cls):
+        """
+        Provides a class method to create an instance with default parameters. All paths
+        are set to empty strings, `time_keyword=FrameReferenceTime`, and `verbose=False`.
 
+        Returns:
+            TACsFromSegmentationStep: A new instance with default parameters.
+        """
+        return cls(input_image_path='',
+                   segmentation_image_path='',
+                   segmentation_label_map_path='',
+                   out_tacs_dir='',
+                   out_tacs_prefix='',
+                   time_keyword='FrameReferenceTime',
+                   verbose=False)
+    
 
 class ResampleBloodTACStep(FunctionBasedStep):
     def __init__(self,
