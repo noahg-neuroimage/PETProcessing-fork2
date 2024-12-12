3a5b5e7f95a95370fc337c665889b52424dfd22c
noahg-neuroimage <noahg@wustl.edu>
2024-12-12 13:24:11 -0600

    Style changes to remove warnings from steps_containers

--- petpal/pipelines/steps_containers.py
+++ petpal/pipelines/steps_containers.py
@@ -1,8 +1,11 @@
+"""
+Manages containers of steps for pipelines.
+"""
 import copy
 from typing import Union
 import networkx as nx
 from matplotlib import pyplot as plt
-from .steps_base import *
+from .steps_base import FunctionBasedStep, ObjectBasedStep
 from .preproc_steps import PreprocStepType, ImageToImageStep, TACsFromSegmentationStep, ResampleBloodTACStep
 from .kinetic_modeling_steps import KMStepType, GraphicalAnalysisStep, TCMFittingAnalysisStep, ParametricGraphicalAnalysisStep
 
@@ -49,7 +52,7 @@ class StepsContainer:
         
         info_str.append(')')
         
-        return f'\n    '.join(info_str)
+        return '\n    '.join(info_str)
     
     def add_step(self, step: StepType):
         """
@@ -86,8 +89,8 @@ class StepsContainer:
             try:
                 del self.step_objs[step]
                 del self.step_names[step]
-            except IndexError:
-                raise IndexError(f"Step number {step} does not exist.")
+            except IndexError as exc:
+                raise IndexError(f"Step number {step} does not exist.") from exc
         elif isinstance(step, str):
             if step not in self.step_names:
                 raise KeyError(f"Step name {step} does not exist.")
@@ -95,9 +98,9 @@ class StepsContainer:
                 step_index = self.step_names.index(step)
                 del self.step_objs[step_index]
                 del self.step_names[step_index]
-            except Exception:
-                raise Exception
-    
+            except Exception as exc:
+                raise Exception from exc
+
     def print_step_details(self):
         """
         Prints the details of all steps in the container.
@@ -113,7 +116,7 @@ class StepsContainer:
                 print("  "+"    \n".join(str(a_step).split('\n')))
                 print('-' * 70)
             print("*" * 80)
-    
+
     def __str__(self) -> str:
         """
         Returns a detailed string representation of the StepsContainer instance.
@@ -131,14 +134,14 @@ class StepsContainer:
                 info_str.append(f"  Step number {step_id + 1:>02}: {step_name}")
             info_str.append('-' * 70)
         return "\n".join(info_str)
-    
+
     def __call__(self):
         """
         Executes all steps in the container in sequence.
         """
-        for step_id, (step_name, a_step) in enumerate(zip(self.step_names, self.step_objs)):
+        for _step_id, (_step_name, a_step) in enumerate(zip(self.step_names, self.step_objs)):
             a_step.execute()
-    
+
     def __getitem__(self, step: Union[int, str]):
         """
         Gets a step from the container by index or name.
@@ -157,19 +160,19 @@ class StepsContainer:
         if isinstance(step, int):
             try:
                 return self.step_objs[step]
-            except IndexError:
-                raise IndexError(f"Step number {step} does not exist.")
+            except IndexError as exc:
+                raise IndexError(f"Step number {step} does not exist.") from exc
         elif isinstance(step, str):
             if step not in self.step_names:
                 raise KeyError(f"Step name {step} does not exist.")
             try:
                 step_index = self.step_names.index(step)
                 return self.step_objs[step_index]
-            except KeyError:
-                raise KeyError(f"Step name {step} does not exist.")
+            except KeyError as exc:
+                raise KeyError(f"Step name {step} does not exist.") from exc
         else:
             raise TypeError(f"Key must be an integer or a string. Got {type(step)}")
-    
+
     def __add__(self, other: 'StepsContainer') -> 'StepsContainer':
         """
         Combines this StepsContainer with another one. The other container cannot have steps with the same name.
@@ -194,7 +197,7 @@ class StepsContainer:
             return new_container
         else:
             raise TypeError("Can only add another StepsContainer instance")
-    
+
     @classmethod
     def default_preprocess_steps(cls, name: str = 'preproc'):
         """
@@ -220,7 +223,7 @@ class StepsContainer:
         obj.add_step(TACsFromSegmentationStep.default_write_tacs_from_segmentation_rois())
         obj.add_step(ResampleBloodTACStep.default_resample_blood_tac_on_scanner_times())
         return obj
-    
+
     @classmethod
     def default_graphical_analysis_steps(cls, name: str = 'km_graphical_analysis'):
         """
@@ -245,7 +248,7 @@ class StepsContainer:
         obj.add_step(GraphicalAnalysisStep.default_logan())
         obj.add_step(GraphicalAnalysisStep.default_alt_logan())
         return obj
-    
+
     @classmethod
     def default_parametric_graphical_analysis_steps(cls, name: str = 'km_parametric_graphical_analysis'):
         """
@@ -270,7 +273,7 @@ class StepsContainer:
         obj.add_step(ParametricGraphicalAnalysisStep.default_logan())
         obj.add_step(ParametricGraphicalAnalysisStep.default_alt_logan())
         return obj
-    
+
     @classmethod
     def default_tcm_analysis_steps(cls, name: str = 'km_tcm_analysis'):
         """
@@ -292,7 +295,7 @@ class StepsContainer:
         obj.add_step(TCMFittingAnalysisStep.default_serial2tcm())
         obj.add_step(TCMFittingAnalysisStep.default_irreversible_2tcm())
         return obj
-    
+
     @classmethod
     def default_kinetic_analysis_steps(cls, name: str = 'km'):
         """
@@ -318,11 +321,11 @@ class StepsContainer:
         Notes:
             The steps do not technically depend on each other and can be run out of sequence.
         """
-        
+
         parametric_graphical_analysis_steps = cls.default_parametric_graphical_analysis_steps()
         graphical_analysis_steps = cls.default_graphical_analysis_steps()
         tcm_analysis_steps = cls.default_tcm_analysis_steps()
-        
+
         obj = parametric_graphical_analysis_steps + graphical_analysis_steps + tcm_analysis_steps
         obj.name = name
         return obj
@@ -354,10 +357,10 @@ class StepsPipeline:
         self.name: str = name
         self.step_containers: dict[str, StepsContainer] = {}
         self.dependency_graph = nx.DiGraph()
-        
+
         for container in step_containers:
             self.add_container(container)
-    
+
     def __repr__(self):
         """
         Provides an unambiguous string representation of the TACsFromSegmentationStep instance.
@@ -370,14 +373,14 @@ class StepsPipeline:
         """
         cls_name = type(self).__name__
         info_str = [f'{cls_name}(', f'name={repr(self.name)},' 'step_containers=[']
-        
+
         for _, container_obj in self.step_containers.items():
             info_str.append(f'{repr(container_obj)},')
         info_str.append(']')
         info_str.append(')')
-        
-        return f'\n    '.join(info_str)
-    
+
+        return '\n    '.join(info_str)
+
     def __str__(self) -> str:
         """
         Returns a detailed string representation of the StepsPipeline instance.
@@ -394,7 +397,7 @@ class StepsPipeline:
                 info_str.append(f"    Step {step_num:>02}: {repr(step_name)}")
                 step_deps = list(self.dependency_graph[step_name].keys())
                 if step_deps:
-                    info_str.append(fr"      \> sends output to:")
+                    info_str.append(r"      \> sends output to:")
                     for dep_name in step_deps[:-1]:
                         info_str.append(fr"            ├── {repr(dep_name)}")
                     for dep_name in step_deps[-1:]:
@@ -402,7 +405,7 @@ class StepsPipeline:
             info_str.append("-" * sep_line_len)
         info_str.append("*" * sep_line_len)
         return "\n".join(info_str)
-    
+
     def add_container(self, step_container: StepsContainer):
         """
         Adds a step container to the pipeline.
@@ -416,17 +419,17 @@ class StepsPipeline:
         """
         if not isinstance(step_container, StepsContainer):
             raise TypeError("`step_container` must be an instance of StepsContainer")
-        
+
         container_name = step_container.name
-        if container_name in self.step_containers.keys():
+        if container_name in self.step_containers:
             raise KeyError(f"Container name {container_name} already exists.")
-        
+
         self.step_containers[container_name] = copy.deepcopy(step_container)
-        
+
         step_objs = self.step_containers[container_name].step_objs
         for step in step_objs:
             self.dependency_graph.add_node(f"{step.name}", grp=container_name)
-    
+
     def __call__(self):
         """
         Executes all steps in the pipeline in topologically sorted order.
@@ -434,7 +437,7 @@ class StepsPipeline:
         for step_name in nx.topological_sort(self.dependency_graph):
             step = self.get_step_from_node_label(node_label=step_name)
             step.execute()
-    
+
     def add_step(self, container_name: str, step: StepType):
         """
         Adds a step to a specified container in the pipeline.
@@ -446,14 +449,14 @@ class StepsPipeline:
         Raises:
             KeyError: If the container or step name does not exist or if the step name already exists.
         """
-        if container_name not in self.step_containers.keys():
+        if container_name not in self.step_containers:
             raise KeyError(f"Container name {container_name} does not exist.")
         if step.name in self.dependency_graph:
             raise KeyError(f"Step name {step.name} already exists. Steps must have unique names.")
-        
+
         self.step_containers[container_name].add_step(step)
         self.dependency_graph.add_node(f"{step.name}", grp=container_name)
-    
+
     def remove_step(self, step: str):
         """
         Removes a step from the pipeline based on its name.
@@ -471,7 +474,7 @@ class StepsPipeline:
         container_name = graph_nodes[step]['grp']
         self.dependency_graph.remove_node(step)
         self.step_containers[container_name].remove_step(step)
-    
+
     def print_steps_names(self, container_name: Union[str, None] = None):
         """
         Prints the names of all steps in the pipeline or a specific container.
@@ -484,13 +487,13 @@ class StepsPipeline:
             KeyError: If the container name does not exist.
         """
         if container_name is None:
-            for name, container in self.step_containers.items():
+            for _name, container in self.step_containers.items():
                 print(str(container))
-        elif container_name in self.step_containers.keys():
+        elif container_name in self.step_containers:
             print(str(self.step_containers[container_name]))
         else:
             raise KeyError(f"Container name {container_name} does not exist. ")
-    
+
     def print_steps_details(self, container_name: Union[str, None] = None):
         """
         Prints the details of all steps in the pipeline or a specific container.
@@ -503,13 +506,13 @@ class StepsPipeline:
             KeyError: If the container name does not exist.
         """
         if container_name is None:
-            for name, container in self.step_containers.items():
+            for _name, container in self.step_containers.items():
                 container.print_step_details()
-        elif container_name in self.step_containers.keys():
+        elif container_name in self.step_containers:
             self.step_containers[container_name].print_step_details()
         else:
             raise KeyError(f"Container name {container_name} does not exist. ")
-    
+
     def add_dependency(self, sending: str, receiving: str):
         """
         Adds a dependency between two steps in the pipeline.
@@ -527,12 +530,12 @@ class StepsPipeline:
             raise KeyError(f"Step name {sending} does not exist.")
         if receiving not in node_names:
             raise KeyError(f"Step name {receiving} does not exist.")
-        
+
         self.dependency_graph.add_edge(sending, receiving)
-        
+
         if not nx.is_directed_acyclic_graph(self.dependency_graph):
             raise RuntimeError(f"Adding dependency {sending} -> {receiving} creates a cycle!")
-    
+
     def get_step_from_node_label(self, node_label: str) -> StepType:
         """
         Retrieves a step object based on its node label in the dependency graph.
@@ -551,7 +554,7 @@ class StepsPipeline:
         graph_nodes = self.dependency_graph.nodes(data=True)
         container_name = graph_nodes[node_label]['grp']
         return self.step_containers[container_name][node_label]
-    
+
     def update_dependencies_for(self, step_name: str, verbose=False):
         """
         Updates the dependencies for a specified step, setting inputs from the outputs of its dependencies.
@@ -572,7 +575,7 @@ class StepsPipeline:
             else:
                 if verbose:
                     print(f"Updated input-output dependency between {sending_step.name} and {receiving_step.name}")
-    
+
     def update_dependencies(self, verbose=False):
         """
         Updates the dependencies for all steps in the pipeline.
@@ -582,7 +585,7 @@ class StepsPipeline:
         """
         for step_name in nx.topological_sort(self.dependency_graph):
             self.update_dependencies_for(step_name=step_name, verbose=verbose)
-    
+
     def get_steps_potential_run_state(self) -> dict:
         """
         Gets a dictionary representing the potential run state of all steps in topologically sorted order.
@@ -595,7 +598,7 @@ class StepsPipeline:
             step = self.get_step_from_node_label(node_name)
             step_maybe_runnable[node_name] = step.can_potentially_run()
         return step_maybe_runnable
-    
+
     def can_steps_potentially_run(self) -> bool:
         """
         Checks if all steps in the pipeline can potentially run.
@@ -604,11 +607,11 @@ class StepsPipeline:
             bool: True if all steps can potentially run, False otherwise.
         """
         steps_run_state = self.get_steps_potential_run_state()
-        for step_name, run_state in steps_run_state.items():
+        for _step_name, run_state in steps_run_state.items():
             if not run_state:
                 return False
         return True
-    
+
     def plot_dependency_graph(self,
                               figsize: tuple = (12, 12),
                               node_options: Union[dict, None] = None,
@@ -642,40 +645,40 @@ class StepsPipeline:
             
         """
         dep_graph = self.dependency_graph
-        
+
         for layer, nodes in enumerate(nx.topological_generations(dep_graph)):
             for node in nodes:
                 dep_graph.nodes[node]['layer'] = layer
-        
+
         dep_pos = nx.multipartite_layout(dep_graph, subset_key='layer', align='horizontal')
         dep_labels = {a_lab: '\n'.join(a_lab.split("_")) for a_lab in dep_graph.nodes}
-        
+
         default_node_options = {'node_size': 2000, 'node_color': 'w', 'node_shape': 'o'}
-        
+
         default_label_options = {'verticalalignment': 'center',
                                  'clip_on': False,
                                  'font_size': 15,
                                  'bbox' : dict(facecolor='azure', edgecolor='blue',
                                                boxstyle='round', alpha=0.25, linewidth=2)
                                 }
-        
+
         default_draw_options = {'with_labels': True, 'arrows': True, 'arrowsize': 30, 'width': 2}
-        
+
         if node_options:
             default_node_options = {**default_node_options, **node_options}
         if label_options:
             default_label_options = {**default_label_options, **label_options}
         if draw_options:
             default_draw_options = {**default_draw_options, **draw_options}
-        
-        myFig, myAx = plt.subplots(figsize=figsize, constrained_layout=True)
+
+        _myFig, myAx = plt.subplots(figsize=figsize, constrained_layout=True)
         nx.draw_networkx(dep_graph, ax=myAx, pos=dep_pos, labels=dep_labels,
                          **default_node_options,
                          **default_label_options,
                          **default_draw_options)
-        
+
         plt.show()
-    
+
     def print_dependency_graph(self):
         """
         Prints a textual representation of the dependency graph for the steps pipeline.
@@ -697,11 +700,11 @@ class StepsPipeline:
                     info_str.append(f"  └── {repr(dep_name)}")
             else:
                 info_str[-1] = f"{info_str[-1]} has no output dependencies"
-        
+
         info_str.append('*' * sep_line_len)
         print('\n'.join(info_str))
-        
-    
+
+
     @classmethod
     def default_steps_pipeline(cls, name='PET-MR_analysis'):
         """
@@ -739,18 +742,18 @@ class StepsPipeline:
         """
         obj = cls(name=name, step_containers=[StepsContainer.default_preprocess_steps(name='preproc'),
                                               StepsContainer.default_kinetic_analysis_steps(name='km')])
-        
+
         obj.add_dependency(sending='thresh_crop', receiving='moco_frames_above_mean')
         obj.add_dependency(sending='moco_frames_above_mean', receiving='register_pet_to_t1')
         obj.add_dependency(sending='register_pet_to_t1', receiving='write_roi_tacs')
         obj.add_dependency(sending='register_pet_to_t1', receiving='resample_PTAC_on_scanner')
-        
+
         for method in ['patlak', 'logan', 'alt_logan']:
             obj.add_dependency(sending='register_pet_to_t1', receiving=f'parametric_{method}_fit')
             obj.add_dependency(sending='resample_PTAC_on_scanner', receiving=f'parametric_{method}_fit')
-        
+
         for fit_model in ['1tcm', '2tcm-k4zero', 'serial-2tcm', 'patlak', 'logan', 'alt_logan']:
             obj.add_dependency(sending='write_roi_tacs', receiving=f"roi_{fit_model}_fit")
             obj.add_dependency(sending='resample_PTAC_on_scanner', receiving=f"roi_{fit_model}_fit")
-        
+
         return obj
