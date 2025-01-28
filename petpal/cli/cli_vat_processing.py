83caff09c305551cd382042b3fadf17c8e50dc2d
noahg-neuroimage <noahg@wustl.edu>
2025-01-28 14:17:15 -0600

    Replace existing VAT CLI with messy working file.

--- petpal/cli/cli_vat_processing.py
+++ petpal/cli/cli_vat_processing.py
@@ -1,52 +1,95 @@
-"""
-Run dynamic VAT PET processing through standard protocol. Handles SUVR parametric imaging and
-regional TACs for further kinetic analysis. File structure standardized in the NIL Movement
-Disorders section is hard-coded. Requires list of subjects to process in a TSV, as well as paths to
-directories containing registrations, BIDS PET data, and output folder to write results to.
-
-Processing steps run:
-    * Motion correction
-    * Registration to MPRAGE
-    * Generation of standard VAT white matter reference region
-    * Write regional TACs to file
-    * Calculation of SUVR
-    * Blur output
-
-This code does not handle:
-    * PVC
-    * Warp to atlas
-
-"""
+# pylint: skip-file
+# V6: Change logan to patlak. Can't run PVC on patlak ki. Fix tac naming.
 import os
 import argparse
+import glob
 import pandas as pd
-from petpal.preproc import preproc
+import numpy as np
+import ants
+from petpal.preproc import preproc, motion_corr
+from petpal.kinetic_modeling import parametric_images, fit_tac_with_rtms, reference_tissue_models, graphical_analysis
 
-_VAT_EXAMPLE_ = r"""
+
+_VAT_EXAMPLE_ = (r"""
 Example:
   - Running many subjects:
     petpal-vat-proc --subjects participants.tsv --out-dir /path/to/output --pet-dir /path/to/pet/folder/ --reg-dir /path/to/subject/Registrations/
-"""
+""")
+def gw_segmentation(freesurfer_path: str,
+                    dseg_path: str,
+                    output_path: str):
+    dseg = pd.read_csv(dseg_path,sep=r'\s+')
+    freesurfer = ants.image_read(freesurfer_path)
+    freesurfer_np = freesurfer.numpy()
+    gm_map = np.zeros_like(freesurfer_np)
+    wm_map = np.zeros_like(freesurfer_np)
+
+    for i,mapping in enumerate(dseg['mapping']):
+        gw_label = dseg['gray_white_matter'].iloc[i]
+        region_seg = np.where(freesurfer_np==mapping)
+        if gw_label == 0:
+            gm_map[region_seg] = 1
+        elif gw_label == 1:
+            wm_map[region_seg] = 1
+
+    gm_img = ants.from_numpy(data=gm_map,
+                             origin=freesurfer.origin,
+                             spacing=freesurfer.spacing,
+                             direction=freesurfer.direction)
+    wm_img = ants.from_numpy(data=wm_map,
+                             origin=freesurfer.origin,
+                             spacing=freesurfer.spacing,
+                             direction=freesurfer.direction)
+    gw_map_template = motion_corr._gen_nd_image_based_on_image_list([gm_img,wm_img])
+    gw_map_4d = ants.list_to_ndimage(image=gw_map_template,image_list=[gm_img,wm_img])
+    ants.image_write(gw_map_4d,output_path)
+
+
+def subcortical_mask(input_seg_path, keeps, output_seg_path):
+    seg = ants.image_read(input_seg_path)
+    seg_np = seg.numpy()
+    scseg = np.zeros(seg_np.shape)
+    for keep in keeps:
+        region_seg = np.where(seg_np==keep)
+        scseg[region_seg] = 1
+    scseg_img = ants.from_numpy(
+        data=scseg,
+        origin=seg.origin,
+        spacing=seg.spacing,
+        direction=seg.direction
+    )
+    ants.image_write(scseg_img,output_seg_path)
+    return 0
+
+
+def k2p_from_mrtm_fit(results):
+    k2_prime = []
+    for result in results:
+        try:
+            k2_prime += [reference_tissue_models.calc_k2prime_from_mrtm_2003_fit(result[0])]
+        except:
+            k2_prime += [np.nan]
+    return k2_prime
+
+
+def ki_from_mrtm_fit(results):
+    ki = []
+    for result in results:
+        try:
+            ki += [reference_tissue_models.calc_ki_from_mrtm_2003_fit(result[0])]
+        except:
+            ki += [np.nan]
+    return ki
 
 
 def vat_protocol(subjstring: str,
                  out_dir: str,
                  pet_dir: str,
-                 reg_dir: str):
-    """
-    Set up parameters necessary to run VAT processing and run steps in order.
-
-    Arguments:
-        subjstring (str): Original subject ID used in study, not BIDS name.
-        out_dir (str): Path to folder to which results are written.
-        pet_dir (str): BIDS folder containing PET data.
-        reg_dir (str): Folder containing standard registrations for all subjects.
-    """
+                 reg_dir: str,
+                 skip: bool):
     sub, ses = rename_subs(subjstring)
     preproc_props = {
-        'FilePathLabelMap': '/data/jsp/human2/goldmann/dseg.tsv',
-        'FilePathAtlas': '/data/petsun43/data1/atlas/MNI152/MNI152_T1_2mm.nii',
-        'FilePathWarpRef': '/data/petsun43/data1/atlas/MNI152/MNI152_T1_2mm.nii',
+        'FilePathLabelMap': '/home/usr/goldmann/dseg.tsv',
         'FilePathFSLPremat': '',
         'FilePathFSLPostmat': '',
         'HalfLife': 6586.2,
@@ -55,24 +98,28 @@ def vat_protocol(subjstring: str,
         'MotionTarget': (0,600),
         'RegPars': {'aff_metric': 'mattes','type_of_transform': 'DenseRigid'},
         'RefRegion': 1,
-        'BlurSize': 6,
+        'BlurSize': 4.2,
         'TimeFrameKeyword': 'FrameTimesStart',
         'Verbose': True
     }
     if ses=='':
         out_folder = f'{out_dir}/{sub}'
-        out_prefix = f'{sub}_pet'
+        out_prefix = f'{sub}'
         preproc_props['FilePathMocoInp'] = f'{pet_dir}/{sub}/pet/{sub}_pet.nii.gz'
         preproc_props['FilePathSeg'] = f'{reg_dir}/{sub}/{sub}_aparc+aseg.nii'
         preproc_props['FilePathBSseg'] = f'{reg_dir}/{sub}/{sub}_brainstem.nii'
         preproc_props['FilePathAnat'] = f'{reg_dir}/{sub}/{sub}_mpr.nii'
+        preproc_props['FilePathWarp'] = f'{reg_dir}/{sub}/PRISMA_TRIO_PIB_NL_ANTS_NoT2/{sub}_mpr_to_PRISMA_TRIO_PIB_NL_T1_ANTSwarp.nii.gz'
+        brain_mask_path = f'{reg_dir}/{sub}/{sub}_mpr_brain_mask.nii'
     else:
         out_folder = f'{out_dir}/{sub}_{ses}'
-        out_prefix = f'{sub}_{ses}_pet'
+        out_prefix = f'{sub}_{ses}'
         preproc_props['FilePathMocoInp'] = f'{pet_dir}/{sub}/{ses}/pet/{sub}_{ses}_trc-18FVAT_pet.nii.gz'
-        preproc_props['FilePathSeg'] = f'{reg_dir}/{sub}_{ses}/{sub}_{ses}_aparc+aseg.nii'
-        preproc_props['FilePathBSseg'] = f'{reg_dir}/{sub}_{ses}/{sub}_{ses}_brainstem.nii'
-        preproc_props['FilePathAnat'] = f'{reg_dir}/{sub}_{ses}/{sub}_{ses}_mpr.nii'
+        preproc_props['FilePathSeg'] = f'{reg_dir}/{subjstring}_Bay3prisma/{subjstring}_Bay3prisma_aparc+aseg.nii'
+        preproc_props['FilePathBSseg'] = f'{reg_dir}/{subjstring}_Bay3prisma/{subjstring}_Bay3prisma_brainstem.nii'
+        preproc_props['FilePathAnat'] = f'{reg_dir}/{subjstring}_Bay3prisma/{subjstring}_Bay3prisma_mpr.nii'
+        preproc_props['FilePathWarp'] = f'{reg_dir}/{subjstring}_Bay3prisma/PRISMA_TRIO_PIB_NL_ANTS_NoT2/{subjstring}_Bay3prisma_mpr_to_PRISMA_TRIO_PIB_NL_T1_ANTSwarp.nii.gz'
+        brain_mask_path = f'{reg_dir}/{subjstring}_Bay3prisma/{subjstring}_Bay3prisma_mpr_brain_mask.nii'
     sub_vat = preproc.PreProc(
         output_directory=out_folder,
         output_filename_prefix=out_prefix
@@ -81,44 +128,120 @@ def vat_protocol(subjstring: str,
         preproc_props['FilePathMocoInp'],
         preproc_props['FilePathSeg'],
         preproc_props['FilePathAnat'],
-        preproc_props['FilePathBSseg']
+        preproc_props['FilePathBSseg'],
+        preproc_props['FilePathWarp'],
+        brain_mask_path
     ]
     for check in real_files:
         if not os.path.exists(check):
             print(f'{check} not found')
             return None
     print(real_files)
+
+
+    if ses=='':
+        sub_flex = sub
+    else:
+        sub_flex = f'{sub}_{ses}'
+
+
+    # preprocessing
     preproc_props['FilePathRegInp'] = sub_vat.generate_outfile_path(method_short='moco')
     sub_vat.update_props(preproc_props)
     sub_vat.run_preproc('motion_corr')
     sub_vat.run_preproc('register_pet')
     sub_vat.run_preproc('vat_wm_ref_region')
+    print('finished wm ref region')
+
+    # write tacs
     preproc_props['FilePathSeg'] = sub_vat.generate_outfile_path(method_short='wm-merged')
-    sub_vat.update_props(preproc_props)
     preproc_props['FilePathTACInput'] = sub_vat.generate_outfile_path(method_short='reg')
+    sub_vat.update_props(preproc_props)
+    sub_vat.run_preproc('write_tacs')
+
+    # run patlak
+    tacs = os.path.join(out_dir,sub_flex,'tacs')
+    graphical_model = graphical_analysis.MultiTACGraphicalAnalysis(
+        input_tac_path=os.path.join(tacs,'WMRef_tac.tsv'),
+        roi_tacs_dir=tacs,
+        output_directory=os.path.join(out_dir,sub_flex),
+        output_filename_prefix=out_prefix,
+        method='patlak',
+        fit_thresh_in_mins=20
+    )
+    graphical_model.run_analysis()
+
+    # calculate pars and save results
+    region_names = [os.path.basename(result['FilePathTTAC']) for result in graphical_model.analysis_props]
+    ki = [result['Slope'] for result in graphical_model.analysis_props]
+    ki_pandas = pd.DataFrame(columns=['regions', 'ki_patlak'],data={'regions': region_names, 'ki_patlak': ki})
+    ki_pandas.to_csv(os.path.join(out_dir,sub_flex,f'{sub_flex}_ki-patlak.tsv'),sep='\t')
+    
+    # parametric patlak
+    parametric_analysis = parametric_images.GraphicalAnalysisParametricImage(
+        input_tac_path=os.path.join(tacs,'WMRef_tac.tsv'),
+        pet4D_img_path=sub_vat.generate_outfile_path(method_short='reg'),
+        output_directory=os.path.join(out_dir,sub_flex),
+        output_filename_prefix=out_prefix,
+    )
+    parametric_analysis.run_analysis(method_name='patlak',t_thresh_in_mins=20,image_scale=1)
+    parametric_analysis.save_analysis()
+
+    # suvr
     preproc_props['FilePathWSSInput'] = sub_vat.generate_outfile_path(method_short='reg')
     preproc_props['FilePathSUVRInput'] = sub_vat.generate_outfile_path(method_short='wss')
     sub_vat.update_props(preproc_props)
-    sub_vat.run_preproc('write_tacs')
     sub_vat.run_preproc('weighted_series_sum')
     sub_vat.run_preproc('suvr')
-    preproc_props['FilePathWarpInput'] = sub_vat.generate_outfile_path(method_short='suvr')
-    preproc_props['FilePathBlurInput'] = sub_vat.generate_outfile_path(method_short='suvr')
-    sub_vat.update_props(preproc_props)
-    sub_vat.run_preproc('gauss_blur')
-    return None
 
+    # pvc
+    segfile_4d = sub_vat.generate_outfile_path(method_short='wm-merged-4d')
+    #suvr_file = sub_vat.generate_outfile_path(method_short='suvr')
+    #suvr_pvc_file = sub_vat.generate_outfile_path(method_short='desc-suvr_pvc-rbv')
+    #os.system(f"/home/usr/odonnellj/PETPVC-build/src/pvc_make4d -i {preproc_props['FilePathSeg']} -o {segfile_4d}")
+    #os.system(f"/home/usr/odonnellj/PETPVC-build/src/petpvc -i {suvr_file} -o {suvr_pvc_file} -m {segfile_4d} -p RBV+VC -x 4.2 -y 4.2 -z 4.2")
+    ki_file = sub_vat.generate_outfile_path(method_short='desc-patlak-slope')
+    #ki_pvc_file = sub_vat.generate_outfile_path(method_short='desc-ki_pvc-rbv')
+    #os.system(f"/home/usr/odonnellj/PETPVC-build/src/pvc_make4d -i {preproc_props['FilePathSeg']} -o {segfile_4d}")
+    #os.system(f"/home/usr/odonnellj/PETPVC-build/src/petpvc -i {ki_file} -o {ki_pvc_file} -m {segfile_4d} -p RBV+VC -x 4.2 -y 4.2 -z 4.2")
+
+
+    # reg ki to atlas
+    ref = ants.image_read('/data/jsp/human2/AaronProjects/PRISMA_TRIO_PIB_NL/PRISMA_TRIO_PIB_NL_MNI152_T1_0p9mm.nii.gz')
+    ki_ants = ants.image_read(ki_file)
+    ki_warp = ants.apply_transforms(
+        fixed=ref,
+        moving=ki_ants,
+        transformlist=[preproc_props['FilePathWarp']],
+        imagetype=0,
+        verbose=1
+    )
+    ki_smooth = ants.smooth_image(image=ki_warp,sigma=4.2,FWHM=True)
+    ants.image_write(ki_smooth,sub_vat.generate_outfile_path(method_short='desc-ki_space-atlas'))
+
+    #suvr_ants = ants.image_read(suvr_pvc_file)
+    #suvr_warp = ants.apply_transforms(
+    #    fixed=ref,
+    #    moving=suvr_ants,
+    #    transformlist=[preproc_props['FilePathWarp']],
+    #    imagetype=0,
+    #    verbose=1
+    #)
+    #suvr_smooth = ants.smooth_image(image=suvr_warp,sigma=6,FWHM=True)
+    #ants.image_write(suvr_smooth,sub_vat.generate_outfile_path(method_short='desc-suvr_pvc-rbv_space-atlas'))
+
+    return None
 
 def rename_subs(sub: str):
     """
-    Handle converting original subject ID to BIDS name.
+    Handle converting subject ID to BIDS structure.
 
-    Arguments:
-        sub (str): Original subject ID
+    VATDYS0XX -> sub-VATDYS0XX
+    PIBXX-YYY_VYrZ -> sub-PIBXXYYY_ses-VYrZ
 
-    Returns:
-        subname (str): Subject portion of ID converted to BIDS format.
-        sesname (str): Session portion of ID converted to BIDS format, if any.
+    returns:
+        - subject part string
+        - session part string
     """
     if 'VAT' in sub:
         return [f'sub-{sub}', '']
@@ -132,7 +255,7 @@ def rename_subs(sub: str):
 
 def main():
     """
-    VAT command line interface. Handles options, loops through subjects and runs.
+    VAT command line interface
     """
     parser = argparse.ArgumentParser(prog='petpal-vat-proc',
                                      description='Command line interface for running VAT processing.',
@@ -143,8 +266,25 @@ def main():
     parser.add_argument('-r','--reg-dir',required=True,help='Path to parent directory of registrations computed from MPR to atlas space.')
     args = parser.parse_args()
 
+
     subs_sheet = pd.read_csv(args.subjects,sep='\t')
     subs = subs_sheet['participant_id']
 
-    for sub in subs:
-        vat_protocol(subjstring=sub,out_dir=args.out_dir,pet_dir=args.pet_dir,reg_dir=args.reg_dir)
+    for sub in subs[:1]:
+        if sub[:6]=='VATDYS':
+            pet_dir = '/data/norris/data1/data_archive/VATDYS'
+            reg_dir = '/export/scratch1/Registration/VATDYS'
+        elif sub[:5]=='VATNL':
+            pet_dir = '/data/norris/data1/data_archive/VATNL'
+            reg_dir = '/data/norris/data1/Registration/VATNL'
+        elif sub[:3]=='PIB':
+            pet_dir = '/data/jsp/human2/BidsDataset/PIB'
+            reg_dir = '/export/scratch1/Maame/Registration/'
+        #vat_protocol(sub,args.out_dir,pet_dir,reg_dir,skip=True)
+        try:
+            vat_protocol(sub,args.out_dir,pet_dir,reg_dir,skip=True)
+        except:
+            print(f"Couldn't run {sub}, passing")
+            pass
+
+main()
